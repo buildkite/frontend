159d64c116e02c79ba7db9932ce1096b0db2188d
Keith Pitt <me@keithpitt.com>
2016-01-11 10:57:44 +0800

    Added a basic navigation component

---
+++ app/components/layout/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Navigation extends React.Component {
+  render() {
+    return (
+      <div>Buildkite Navigation</div>
+    );
+  }
+};
+
+export default Navigation;
