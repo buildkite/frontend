849ac785b7a55e990026c1e896827cfe47db89cd
Tim Lucas <t@toolmantim.com>
2016-02-04 01:18:46 +1100

    Animation and badge tweaks

--- app/components/shared/Badge.js
+++ app/components/shared/Badge.js
@@ -3,8 +3,7 @@ import classNames from 'classnames';
 
 export default function Badge(props) {
   return (
-    <span className={classNames("inline-block bg-black white rounded ml1 small", props.className)}
-          style={{ padding: '2px 4px' }}>
+    <span className={classNames("inline-block bg-black white rounded ml1 small p1 line-height-1", props.className)}>
       {props.children}
     </span>
   );
