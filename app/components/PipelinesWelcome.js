1f1dd3c44c468e4e3aff2e8f41f7508c9c0b34c9
Tim Lucas <t@toolmantim.com>
2016-02-09 14:33:52 +1000

    Animate the pipeline icon

--- app/components/PipelinesWelcome.js
+++ app/components/PipelinesWelcome.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PipelineIcon from './icons/Pipeline';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class PipelinesWelcome extends React.Component {
   static propTypes = {
@@ -12,7 +13,7 @@ class PipelinesWelcome extends React.Component {
         <PipelineIcon />
         <h1 className="h3 m0 mt2 mb4">Create your first pipeline</h1>
         <p className="mx-auto" style={{ maxWidth: "30em" }}>Pipelines define the tasks to be run on your agents. It’s best to keep each pipeline focussed on a single part of your delivery pipeline, such as testing, deployments or infrastructure. Once created, you can connect your pipeline with your source control or trigger it via the API.</p>
-        <p className="gray">Need inspiration? See the <a href="">sample pipelines</a> GitHub repo.</p>
+        <p className="gray">Need inspiration? See the <a target="_blank" href="https://github.com/buildkite/sample-pipelines">sample pipelines</a> GitHub repo.</p>
         <p>
           <a className="mt4 btn bg-lime hover-white white rounded" href={this.props.newUrl}>
             New Pipeline
