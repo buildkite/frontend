6d4c8cc9e1f2b7fc6ac750e02ddd04a9bd36ecba
Keith Pitt <me@keithpitt.com>
2016-03-05 10:36:59 +1100

    Added link support to Button.Menu

--- app/components/organization/SettingsMenu.js
+++ app/components/organization/SettingsMenu.js
@@ -1,86 +1,81 @@
-import { Component, PropTypes } from 'react';
+import { PropTypes } from 'react';
 
 import Menu from '../shared/Menu';
 import Icon from '../shared/Icon';
 import Permissions from '../shared/Permissions';
 
-class SettingsMenu extends Component {
-  static propTypes = {
-    organization: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      slug: PropTypes.string.isRequired,
-      members: PropTypes.shape({
-        count: PropTypes.number.isRequired
+const SettingsMenu = (props) =>
+  <Permissions permissions={props.organization.permissions}>
+    <Menu>
+      <Menu.Header>{props.organization.name}</Menu.Header>
+
+      <Permissions.Only allowed="organizationUpdate">
+        <Menu.Button href={`/organizations/${props.organization.slug}/settings`}>
+          <Icon icon="settings" className="icon-mr"/>Settings
+        </Menu.Button>
+      </Permissions.Only>
+
+      <Permissions.Only allowed="organizationMemberCreate">
+        <Menu.Button href={`/organizations/${props.organization.slug}/users`} badge={props.organization.members.count + props.organization.invitations.count}>
+          <Icon icon="users"className="icon-mr"/>Users
+        </Menu.Button>
+      </Permissions.Only>
+
+      <Permissions.Only allowed="teamAdmin">
+        <Menu.Button link={`/organizations/${props.organization.slug}/teams`}>
+          <Icon icon="teams" className="icon-mr"/>Teams
+        </Menu.Button>
+      </Permissions.Only>
+
+      <Permissions.Only allowed="notificationServiceUpdate">
+        <Menu.Button href={`/organizations/${props.organization.slug}/services`}>
+          <Icon icon="notification-services" className="icon-mr"/>Notification Services
+        </Menu.Button>
+      </Permissions.Only>
+
+      <Permissions.Only allowed="organizationBillingUpdate">
+        <Menu.Button href={`/organizations/${props.organization.slug}/billing`}>
+          <Icon icon="billing" className="icon-mr"/>Billing
+        </Menu.Button>
+      </Permissions.Only>
+    </Menu>
+
+    <Menu>
+      <Menu.Button href={`/user/settings`}>Personal Settings</Menu.Button>
+    </Menu>
+  </Permissions>
+
+SettingsMenu.propTypes = {
+  organization: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    members: PropTypes.shape({
+      count: PropTypes.number.isRequired
+    }).isRequired,
+    invitations: PropTypes.shape({
+      count: PropTypes.number.isRequired
+    }).isRequired,
+    teams: PropTypes.shape({
+      count: PropTypes.number.isRequired
+    }).isRequired,
+    permissions: PropTypes.shape({
+      organizationUpdate: PropTypes.shape({
+        allowed: PropTypes.bool.isRequired
       }).isRequired,
-      invitations: PropTypes.shape({
-        count: PropTypes.number.isRequired
+      organizationMemberCreate: PropTypes.shape({
+        allowed: PropTypes.bool.isRequired
       }).isRequired,
-      teams: PropTypes.shape({
-        count: PropTypes.number.isRequired
+      notificationServiceUpdate: PropTypes.shape({
+        allowed: PropTypes.bool.isRequired
       }).isRequired,
-      permissions: PropTypes.shape({
-        organizationUpdate: PropTypes.shape({
-          allowed: PropTypes.bool.isRequired
-        }).isRequired,
-        organizationMemberCreate: PropTypes.shape({
-          allowed: PropTypes.bool.isRequired
-        }).isRequired,
-        notificationServiceUpdate: PropTypes.shape({
-          allowed: PropTypes.bool.isRequired
-        }).isRequired,
-        organizationBillingUpdate: PropTypes.shape({
-          allowed: PropTypes.bool.isRequired
-        }).isRequired,
-        teamAdmin: PropTypes.shape({
-          allowed: PropTypes.bool.isRequired
-        }).isRequired
-      })
+      organizationBillingUpdate: PropTypes.shape({
+        allowed: PropTypes.bool.isRequired
+      }).isRequired,
+      teamAdmin: PropTypes.shape({
+        allowed: PropTypes.bool.isRequired
+      }).isRequired
     })
-  };
-
-  render() {
-    return (
-      <Permissions permissions={this.props.organization.permissions}>
-        <Menu>
-          <Menu.Header>{this.props.organization.name}</Menu.Header>
-
-          <Permissions.Only allowed="organizationUpdate">
-            <Menu.Button href={`/organizations/${this.props.organization.slug}/settings`}>
-              <Icon icon="settings" className="icon-mr"/>Settings
-            </Menu.Button>
-          </Permissions.Only>
-
-          <Permissions.Only allowed="organizationMemberCreate">
-            <Menu.Button href={`/organizations/${this.props.organization.slug}/users`} badge={this.props.organization.members.count + this.props.organization.invitations.count}>
-              <Icon icon="users"className="icon-mr"/>Users
-            </Menu.Button>
-          </Permissions.Only>
-
-          <Permissions.Only allowed="teamAdmin">
-            <Menu.Button href={`/organizations/${this.props.organization.slug}/teams`}>
-              <Icon icon="teams" className="icon-mr"/>Teams
-            </Menu.Button>
-          </Permissions.Only>
-
-          <Permissions.Only allowed="notificationServiceUpdate">
-            <Menu.Button href={`/organizations/${this.props.organization.slug}/services`}>
-              <Icon icon="notification-services" className="icon-mr"/>Notification Services
-            </Menu.Button>
-          </Permissions.Only>
-
-          <Permissions.Only allowed="organizationBillingUpdate">
-            <Menu.Button href={`/organizations/${this.props.organization.slug}/billing`}>
-              <Icon icon="billing" className="icon-mr"/>Billing
-            </Menu.Button>
-          </Permissions.Only>
-        </Menu>
-
-        <Menu>
-          <Menu.Button href={`/user/settings`}>Personal Settings</Menu.Button>
-        </Menu>
-      </Permissions>
-    );
-  }
-}
+  })
+};
 
 export default SettingsMenu;
--- app/components/shared/Menu/button.js
+++ app/components/shared/Menu/button.js
@@ -1,11 +1,12 @@
 import React from "react";
 import classNames from 'classnames';
+import { Link } from 'react-router';
 
 import Badge from "../Badge";
 
 const COLOR = (Features.NewNav) ? "lime" : "blue";
 
-const Button = (props) => {
+const Button = (props, context) => {
   let active = (window.location.pathname.indexOf(props.href) == 0)
 
   let badge;
@@ -23,15 +24,30 @@ const Button = (props) => {
     "blue": (active && COLOR == "blue")
   })
 
-  return (
-    <a href={props.href} className={buttonClasses}>{props.children}{badge}</a>
-  );
+  // If we've defined a link instead of a href (a link is used to navigate
+  // through react-router, instead of a regular href) and context.router is
+  // present (which means the routing gear has been activated) then create a
+  // react-router link - otherwise, just fallback to a regular href.
+  if(props.link && context.router) {
+    return (
+      <Link to={props.link} className={buttonClasses}>{props.children}{badge}</Link>
+    );
+  } else {
+    return (
+      <a href={props.link || props.href} className={buttonClasses}>{props.children}{badge}</a>
+    );
+  }
 }
 
 Button.propTypes = {
   children: React.PropTypes.node.isRequired,
   badge: React.PropTypes.number,
-  href: React.PropTypes.string
+  href: React.PropTypes.string,
+  link: React.PropTypes.string
+};
+
+Button.contextTypes = {
+  router: React.PropTypes.object
 };
 
 Button.displayName = "Menu.Button";
