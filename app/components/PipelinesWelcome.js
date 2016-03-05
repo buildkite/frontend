b5d45cb537de6326d6e2110db1e27d96a344fb35
Keith Pitt <me@keithpitt.com>
2016-03-06 10:21:50 +1100

    More pure-function conversions

--- app/components/PipelinesWelcome.js
+++ app/components/PipelinesWelcome.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import PipelineIcon from './icons/Pipeline';
 
-class PipelinesWelcome extends React.Component {
-  static propTypes = {
-    newUrl: React.PropTypes.string.isRequired
-  };
+const PipelinesWelcome = (props) =>
+  <div className="center p4">
+    <PipelineIcon />
+    <h1 className="h3 m0 mt2 mb4">Create your first pipeline</h1>
+    <p className="mx-auto" style={{ maxWidth: "30em" }}>Pipelines define the tasks to be run on your agents. It’s best to keep each pipeline focussed on a single part of your delivery pipeline, such as testing, deployments or infrastructure. Once created, you can connect your pipeline with your source control or trigger it via the API.</p>
+    <p className="dark-gray">Need inspiration? See the <a target="_blank" href="https://github.com/buildkite/sample-pipelines">sample pipelines</a> GitHub repo.</p>
+    <p>
+      <a className="mt4 btn btn-primary bg-lime hover-white white rounded" href={props.newUrl}>New Pipeline</a>
+    </p>
+  </div>
 
-  render() {
-    return (
-      <div className="center p4">
-        <PipelineIcon />
-        <h1 className="h3 m0 mt2 mb4">Create your first pipeline</h1>
-        <p className="mx-auto" style={{ maxWidth: "30em" }}>Pipelines define the tasks to be run on your agents. It’s best to keep each pipeline focussed on a single part of your delivery pipeline, such as testing, deployments or infrastructure. Once created, you can connect your pipeline with your source control or trigger it via the API.</p>
-        <p className="gray">Need inspiration? See the <a target="_blank" href="https://github.com/buildkite/sample-pipelines">sample pipelines</a> GitHub repo.</p>
-        <p>
-          <a className="mt4 btn bg-lime hover-white white rounded" href={this.props.newUrl}>
-            New Pipeline
-          </a>
-        </p>
-      </div>
-    );
-  }
-}
+PipelinesWelcome.propTypes = {
+  newUrl: React.PropTypes.string.isRequired
+};
 
 export default PipelinesWelcome;
