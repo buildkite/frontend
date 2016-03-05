ba4be217fc0d551432a3450c1a834aac34ec77e9
Keith Pitt <me@keithpitt.com>
2016-03-05 12:54:24 +1100

    Added a spinner to the PageLoader component

--- app/components/shared/PageLoader.js
+++ app/components/shared/PageLoader.js
@@ -3,7 +3,9 @@ import React from 'react';
 class PageLoader extends React.Component {
   render() {
     return (
-      <div>Loading...</div>
+      <div style={{fontSize: "30px", color: "#989898", textAlign: "center", marginTop: "30px"}}>
+	<i className="fa fa-spin fa-spinner"></i>
+      </div>
     );
   }
 }
