1a79b11c9d83e851937553f9eedecdc9a8577cfb
Keith Pitt <me@keithpitt.com>
2016-03-08 00:38:04 +1100

    Added a new SectionLoader and updated the PageLoader

---
+++ app/components/shared/SectionLoader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SectionLoader extends React.Component {
+  render() {
+    return (
+      <div style={{fontSize: "15px", color: "#989898", textAlign: "center", marginTop: "30px"}}>Loading section...</div>
+    );
+  }
+}
+
+export default SectionLoader
