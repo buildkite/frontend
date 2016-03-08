2c601f34fde428817e9f86660674df658e326b4f
Keith Pitt <me@keithpitt.com>
2016-03-09 10:38:32 +1100

    Handle selected styling in the pipeline row

--- app/components/team/Pipelines/pipeline.js
+++ app/components/team/Pipelines/pipeline.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import classNames from 'classnames';
 
 class Pipeline extends React.Component {
   static displayName = "Team.Pipelines.Pipeline";
 
+  static contextTypes = {
+    autoCompletorSuggestion: React.PropTypes.object
+  };
+
   render() {
+    // Toggle the `dark-gray` color on the repository text if this component is
+    // in an auto completor and is highlighted.
+    let autoCompletorSuggestion = this.context.autoCompletorSuggestion;
+    let repositoryTextClasses = classNames({
+      "dark-gray": !autoCompletorSuggestion || (autoCompletorSuggestion && !autoCompletorSuggestion.selected),
+      "white": (autoCompletorSuggestion && autoCompletorSuggestion.selected)
+    });
+
     return (
       <div>
-	<strong className="text-semi-bold">{this.props.pipeline.name}</strong><br/>
-	<small className="text-half-muted">{this.props.pipeline.repository}</small>
+	<strong className="semi-bold block">{this.props.pipeline.name}</strong>
+	<small className={repositoryTextClasses}>{this.props.pipeline.repository}</small>
       </div>
     );
   }
