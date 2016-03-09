40d67f79c915a4446de5a3ff321e6f4ce68a4469
Keith Pitt <me@keithpitt.com>
2016-03-09 17:03:22 +1100

    Added back teams to the organization menu

--- app/components/organization/SettingsMenu.js
+++ app/components/organization/SettingsMenu.js
@@ -25,6 +25,9 @@ class SettingsMenu extends React.Component {
         organizationMemberCreate: React.PropTypes.shape({
           allowed: React.PropTypes.bool.isRequired
         }).isRequired,
+        teamAdmin: React.PropTypes.shape({
+          allowed: React.PropTypes.bool.isRequired
+        }).isRequired,
         notificationServiceUpdate: React.PropTypes.shape({
           allowed: React.PropTypes.bool.isRequired
         }).isRequired,
@@ -65,6 +68,14 @@ class SettingsMenu extends React.Component {
         render: (idx) => (
           <Menu.Button key={idx} href={`/organizations/${this.props.organization.slug}/users`} badge={this.props.organization.members.count + this.props.organization.invitations.count}>
             <Icon icon="users" className="icon-mr"/>Users
+          </Menu.Button>
+        )
+      },
+      {
+        allowed: "teamAdmin",
+        render: (idx) => (
+          <Menu.Button key={idx} link={`/organizations/${this.props.organization.slug}/teams`} badge={this.props.organization.teams.count}>
+            <Icon icon="teams" className="icon-mr"/>Teams
           </Menu.Button>
         )
       },
