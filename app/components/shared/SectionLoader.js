be241db4e1cbd8b1a9909feb018503c316a6d975
Keith Pitt <me@keithpitt.com>
2016-03-12 21:46:52 +1100

    Tweaked the section loader text

--- app/components/shared/SectionLoader.js
+++ app/components/shared/SectionLoader.js
@@ -3,7 +3,7 @@ import React from 'react';
 class SectionLoader extends React.Component {
   render() {
     return (
-      <div style={{fontSize: "15px", color: "#989898", textAlign: "center", marginTop: "30px"}}>Loading section...</div>
+      <div style={{fontSize: "15px", color: "#989898", textAlign: "center", marginTop: "30px"}}>Loading...</div>
     );
   }
 }
