7ea86fbb02b97d880e16c0fecd96b977762f32be
Keith Pitt <me@keithpitt.com>
2016-03-13 10:27:45 +1100

    Show the organization settings button if any of the permissions are true

--- app/components/layout/Navigation/index.js
+++ app/components/layout/Navigation/index.js
@@ -195,7 +195,7 @@ class Navigation extends React.Component {
 
     return permissions(organization.permissions).first(
       {
-        all: true,
+        any: true,
         render: () => <NavigationButton href={`/organizations/${organization.slug}/settings`}>Settings</NavigationButton>
       }
     )
