f37a0b43862fe3defb4a55e46985db4c17b2bba1
Tim Lucas <t@toolmantim.com>
2016-03-30 20:57:22 +1100

    Friendlier team form title

--- app/components/team/New.js
+++ app/components/team/New.js
@@ -35,7 +35,7 @@ class New extends React.Component {
       <DocumentTitle title={`New Team · ${this.props.organization.name}`}>
         <form onSubmit={this.handleFormSubmit}>
           <PageHeader>
-            <PageHeader.Title>Create New Team</PageHeader.Title>
+            <PageHeader.Title>Create a Team</PageHeader.Title>
           </PageHeader>
 
           <Panel>
