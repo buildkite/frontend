5c772d27b2f383444fd52a89e29b7e047694dd41
Keith Pitt <me@keithpitt.com>
2016-05-30 11:55:18 +1000

    Smaller team desc text in the dropdown.

--- app/components/organization/Teams/index.js
+++ app/components/organization/Teams/index.js
@@ -48,7 +48,7 @@ class Teams extends React.Component {
     return this.props.organization.teams.edges.map((edge) =>
       <Chooser.Option key={edge.node.id} value={edge.node.slug} className="btn block hover-bg-silver">
         <Emojify className="block" text={edge.node.name} />
-        {edge.node.description ? <Emojify className="dark-gray light" text={edge.node.description} /> : null}
+        {edge.node.description ? <Emojify className="dark-gray light small" text={edge.node.description} /> : null}
       </Chooser.Option>
     )
   }
