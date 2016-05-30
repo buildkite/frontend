46233412d3b4e228073b3602bed98816791e0b61
Keith Pitt <me@keithpitt.com>
2016-05-30 15:48:32 +1000

    Use the newer prefxied PIPELINE_ACCESS_LEVEL enum

--- app/components/team/Pipelines/access-level.js
+++ app/components/team/Pipelines/access-level.js
@@ -5,6 +5,10 @@ import Chooser from '../../shared/Chooser';
 import Media from '../../shared/Media';
 import Dropdown from '../../shared/Dropdown';
 
+const MANAGE_BUILD_AND_READ = "PIPELINE_ACCESS_LEVEL_MANAGE_BUILD_AND_READ";
+const BUILD_AND_READ = "PIPELINE_ACCESS_LEVEL_BUILD_AND_READ";
+const READ_ONLY = "PIPELINE_ACCESS_LEVEL_READ_ONLY";
+
 class AccessLevel extends React.Component {
   static displayName = "Team.Pipelines.AccessLevel";
 
@@ -19,13 +23,13 @@ class AccessLevel extends React.Component {
   render() {
     let label;
     let nibOffset;
-    if(this.props.teamPipeline.accessLevel == "MANAGE_BUILD_AND_READ") {
+    if(this.props.teamPipeline.accessLevel == MANAGE_BUILD_AND_READ) {
       label = "Full Access"
       nibOffset = 10;
-    } else if (this.props.teamPipeline.accessLevel == "BUILD_AND_READ") {
+    } else if (this.props.teamPipeline.accessLevel == BUILD_AND_READ) {
       label = "Build & Read"
       nibOffset = 3;
-    } else if (this.props.teamPipeline.accessLevel == "READ_ONLY") {
+    } else if (this.props.teamPipeline.accessLevel == READ_ONLY) {
       label = "Read Only";
       nibOffset = 13;
     }
@@ -37,9 +41,9 @@ class AccessLevel extends React.Component {
         </div>
 
         <Chooser selected={this.props.teamPipeline.accessLevel} onSelect={this.props.onAccessLevelChange}>
-          <Chooser.Option value="MANAGE_BUILD_AND_READ" className="btn block hover-bg-silver">
+          <Chooser.Option value={MANAGE_BUILD_AND_READ} className="btn block hover-bg-silver">
             <Media align="top">
-              <Media.Image>{this.renderIcon("MANAGE_BUILD_AND_READ")}</Media.Image>
+              <Media.Image>{this.renderIcon(MANAGE_BUILD_AND_READ)}</Media.Image>
               <Media.Description>
                 <span className="semi-bold block">Full Access</span>
                 <small className="regular dark-gray">Members can edit pipeline settings, view builds and create builds</small>
@@ -47,9 +51,9 @@ class AccessLevel extends React.Component {
             </Media>
           </Chooser.Option>
 
-          <Chooser.Option value="BUILD_AND_READ" className="btn block hover-bg-silver">
+          <Chooser.Option value={BUILD_AND_READ} className="btn block hover-bg-silver">
             <Media align="top">
-              <Media.Image>{this.renderIcon("BUILD_AND_READ")}</Media.Image>
+              <Media.Image>{this.renderIcon(BUILD_AND_READ)}</Media.Image>
               <Media.Description>
                 <span className="semi-bold block">Build &amp; Read</span>
                 <small className="regular dark-gray">Members can view builds and create builds</small>
@@ -57,9 +61,9 @@ class AccessLevel extends React.Component {
             </Media>
           </Chooser.Option>
 
-          <Chooser.Option value="READ_ONLY" className="btn block hover-bg-silver">
+          <Chooser.Option value={READ_ONLY} className="btn block hover-bg-silver">
             <Media align="top">
-              <Media.Image>{this.renderIcon("READ_ONLY")}</Media.Image>
+              <Media.Image>{this.renderIcon(READ_ONLY)}</Media.Image>
               <Media.Description>
                 <span className="semi-bold block">Read Only</span>
                 <small className="regular dark-gray">Members can only view builds</small>
