305e900319e54cf14ed9327f56fbf5dc11f7d8c2
Keith Pitt <me@keithpitt.com>
2016-06-05 15:38:26 +1000

    Added codemirror to the pipeline edit page.

--- app/components/pipeline/Edit.js
+++ app/components/pipeline/Edit.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import Relay from 'react-relay';
+import Codemirror from 'react-codemirror';
+
+import Panel from '../shared/Panel'
+import Button from '../shared/Button'
+
+require('codemirror/mode/yaml/yaml');
+
+const value = `---
+steps:
+  hello: 'great'`
 
 class Edit extends React.Component {
   static propTypes = {
     pipeline: React.PropTypes.object.isRequired
   };
 
+  state = {
+    saving: false,
+    errors: null
+  };
+
   render() {
     return (
-      <div>settings</div>
+      <Panel outline={false}>
+        <Codemirror options={{mode: 'yaml', lineNumbers: true}} value={value} />
+
+        <Panel.Footer>
+          <Button loading={this.state.saving ? "Saving schedule…" : false} theme="success">Save Schedule</Button>
+        </Panel.Footer>
+      </Panel>
     );
   }
 }
