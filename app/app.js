5639d1727bfef171412a2fa5130d844fbd2f6e7d
Keith Pitt <me@keithpitt.com>
2016-06-06 09:52:50 +1000

    Added YML linting support

--- app/app.js
+++ app/app.js
@@ -34,7 +34,11 @@ window["Webpack"] = {
     "eventemitter3": require("eventemitter3"),
     "autosize": require("autosize"),
     "react-codemirror": require("react-codemirror"),
+
     "codemirror/mode/yaml/yaml": require("codemirror/mode/yaml/yaml"),
+    "codemirror/addon/lint/lint": require("codemirror/addon/lint/lint"),
+    "codemirror/addon/lint/yaml-lint": require("codemirror/addon/lint/yaml-lint"),
+    "js-yaml": require("js-yaml"),
 
     "components/layout/Navigation": require("./components/layout/Navigation").default,
     "components/layout/Footer": require("./components/layout/Footer").default,
