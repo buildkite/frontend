4382a1b2783c6da7817b7f122a73b49bc138790f
Keith Pitt <me@keithpitt.com>
2016-06-15 15:38:18 +1000

    Tweaked the default color of the favorite icon

--- app/components/icons/Favorite.js
+++ app/components/icons/Favorite.js
@@ -20,7 +20,7 @@ class Favorite extends React.Component {
       <svg width="20px" height="15px" viewBox="0 0 16 15" onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}>
         <title>Favorite</title>
         <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" fillOpacity="0.3">
-          <g transform="translate(-1157.000000, -134.000000)" stroke={this.props.favorite || this.state.hover ? "#F8CC1C" : "#DDDDDD"} fill={this.props.favorite && !this.state.hover ? "#F8CC1C" : ""}>
+          <g transform="translate(-1157.000000, -134.000000)" stroke={this.props.favorite || this.state.hover ? "#F8CC1C" : "currentColor"} fill={this.props.favorite && !this.state.hover ? "#F8CC1C" : ""}>
             <g transform="translate(54.000000, 115.000000)">
               <polygon points="1111 31 1106.29772 33.472136 1107.19577 28.236068 1103.39155 24.527864 1108.64886 23.763932 1111 19 1113.35114 23.763932 1118.60845 24.527864 1114.80423 28.236068 1115.70228 33.472136"></polygon>
             </g>
