fdcf06d5a8a52fd39fef3b6e599b78c4b4f5aed3
Keith Pitt <me@keithpitt.com>
2016-08-10 15:14:45 +1000

    Added a RelayPreloader class to load teams and organization information

--- app/app.js
+++ app/app.js
@@ -53,6 +53,7 @@ window["Webpack"] = {
     "lib/Logger": require("./lib/Logger").default,
     "lib/Emoji": require("./lib/Emoji").default,
     "lib/RelayBridge": require("./lib/RelayBridge").default,
+    "lib/RelayPreloader": require("./lib/RelayPreloader").default,
     "lib/jobCommandOneliner": require("./lib/jobCommandOneliner").default
   },
 
