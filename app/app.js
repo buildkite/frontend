01a7b7870fd5382d1d5ed0f29edb65c7938dd660
Keith Pitt <me@keithpitt.com>
2016-08-10 11:02:35 +1000

    Renamed `PipelinesWelcome` to `organization/Welcome`

--- app/app.js
+++ app/app.js
@@ -46,7 +46,7 @@ window["Webpack"] = {
     "components/organization/AgentsCount": require("./components/organization/AgentsCount").default,
     "components/organization/SettingsMenu": require("./components/organization/SettingsMenu").default,
     "components/user/SettingsMenu": require("./components/user/SettingsMenu").default,
-    "components/PipelinesWelcome": require("./components/PipelinesWelcome").default,
+    "components/PipelinesWelcome": require("./components/organization/Welcome").default,
     "components/pipeline/Teams": require("./components/pipeline/Teams").default,
     "stores/PusherStore": require("./stores/PusherStore").default,
     "lib/friendlyRelativeTime": require("./lib/friendlyRelativeTime").default,
