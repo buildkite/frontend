4c2d56746067446782763dd557eadc88df184b3f
Tim Lucas <t@toolmantim.com>
2016-08-12 16:59:15 +1000

    No more s3cmd

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -12,7 +12,7 @@ buildkite-agent artifact download "dist/*" "dist/"
 
 echo "--- :s3: Deploying frontend to $S3_URL$WEBPACK_CONFIG_SHA1/"
 
-s3cmd put -P --recursive --verbose --force --no-preserve --exclude="manifest.json" "dist/" "$S3_URL$WEBPACK_CONFIG_SHA1/"
+aws s3 sync --acl "public-read" --exclude="manifest.json" "dist/" "$S3_URL$WEBPACK_CONFIG_SHA1/"
 
 echo "--- :wastebasket: Cleaning up.."
 
@@ -29,7 +29,7 @@ fi
 # Download the files in manifest.json
 for url in $(cat dist/manifest.json | jq -r '.[].js'); do
   # Make sure the URL is prefixed with https (if it's just got //)
-  URL=$(echo $url | sed -e 's/^\/\//https:\/\//')
+  URL=$(echo "$url" | sed -e 's/^\/\//https:\/\//')
 
   pushd "tmp/verify" >> /dev/null
   echo "Downloading $URL"
@@ -41,7 +41,7 @@ echo "--- :mag: Verifiying the files uploaded match the downloads"
 
 # Check that the files are the same
 for f in tmp/verify/*; do
-  NAME=$(basename $f)
+  NAME=$(basename "$f")
 
   DOWNLOADED=$(openssl sha1 "tmp/verify/$NAME" | sed 's/^.* //')
   LOCAL=$(openssl sha1 "dist/$NAME" | sed 's/^.* //')
@@ -69,4 +69,4 @@ echo "--- :s3: Uploading manifest.json and renaming to $MANIFEST_NAME"
 
 # The manifest.json file won't get uploaded to the webpack SHA1'd folder beacuse
 # the name already includes the build commit which is always unique.
-s3cmd put -P --recursive --verbose --force --no-preserve "dist/manifest.json" "$S3_URL$MANIFEST_NAME"
+aws s3 sync --acl "public-read" "dist/manifest.json" "$S3_URL$MANIFEST_NAME"
