3060793cf042fa240c59365844aa6c85bbd87e82
Jessica Stokes <hello@jessicastokes.net>
2016-09-19 15:42:20 +1000

    Improve formatting of token view

--- app/components/agent/Index.js
+++ app/components/agent/Index.js
@@ -74,11 +74,11 @@ class AgentIndex extends React.Component {
                 <Panel.Row>
                   {agentTokens.map((token, index, array) => (
                     <div key={index}>
-                      <small className="dark-gray">{token.node.description}</small><br />
+                      <small className="dark-gray mb1 block">{token.node.description}</small>
                       <RevealButton caption="Reveal Agent Token">
                         <code className="red monospace" style={{ wordWrap: "break-word" }}>{token.node.token}</code>
                       </RevealButton>
-                      {index !== array.length - 1 && <hr className="bg-gray mx-auto max-width-1 border-none height-0" style={{ height: 1 }} />}
+                      {index !== array.length - 1 && <hr className="bg-gray mx-auto max-width-1 border-none height-0 my3" style={{ height: 1 }} />}
                     </div>
                   ))}
                 </Panel.Row>
