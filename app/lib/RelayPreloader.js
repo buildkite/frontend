74f6fcf053c7dd27c87b7af2186244bd7742d68b
Jessica Stokes <hello@jessicastokes.net>
2016-09-21 16:12:50 +1000

    Add preloader queries for navigation

--- app/lib/RelayPreloader.js
+++ app/lib/RelayPreloader.js
@@ -25,6 +25,71 @@ const QUERIES = {
         }
       }
     }
+  `,
+  "navigation/organization": Relay.QL`
+    query NavigationOrganization($organization: ID!) {
+      organization(slug: $organization) {
+        name
+        id
+        slug
+        agents {
+          count
+        }
+        permissions {
+          organizationUpdate {
+            allowed
+          }
+          organizationMemberCreate {
+            allowed
+          }
+          notificationServiceUpdate {
+            allowed
+          }
+          organizationBillingUpdate {
+            allowed
+          }
+          teamAdmin {
+            allowed
+          }
+        }
+      }
+    }
+  `,
+  "navigation/viewer": Relay.QL`
+    query NavigationViewer {
+      viewer {
+        user {
+          name,
+          avatar {
+            url
+          }
+          id
+        }
+        organizations(first: 500) {
+          edges {
+            node {
+              slug,
+              name,
+              id
+            }
+            cursor
+          }
+          pageInfo {
+            hasNextPage,
+            hasPreviousPage
+          }
+        }
+        unreadChangelogs: changelogs(read: false) {
+          count
+        }
+        runningBuilds: builds(state: BUILD_STATE_RUNNING) {
+          count
+        }
+        scheduledBuilds: builds(state: BUILD_STATE_SCHEDULED) {
+          count
+        }
+      }
+    }
   `
 };
 
