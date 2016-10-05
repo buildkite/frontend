22fb7b38761e21acd90baa1f9c50fb46caed7f10
Jessica Stokes <hello@jessicastokes.net>
2016-10-05 12:54:16 +1100

    Work around a Jest bug
    For context: facebook/hest#1864

--- .buildkite/jest.sh
+++ .buildkite/jest.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 echo "+++ :jest: Running Jest"
-npm run test --silent
+npm run test --silent | cat # remove 🐈 when this patch (https://github.com/facebook/jest/pull/1864) lands in Jest
 
 echo "👌 Looks good to me!"
