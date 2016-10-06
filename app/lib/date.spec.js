9bc8d0a980c40d7cbea02e62049a962d1d8c869f
Jessica Stokes <hello@jessicastokes.net>
2016-10-06 17:39:29 +1100

    Make specs resilient to the horror of time zones

--- app/lib/date.spec.js
+++ app/lib/date.spec.js
@@ -3,14 +3,14 @@ import MockDate from 'mockdate';
 import { getDurationString } from './date';
 
 const DATE_FIXTURES = [
-  { from: "2016-05-07T09:00:00.000Z", to: "2016-05-07T09:00:00.000Z" },
-  { from: "2016-05-07T09:00:00.000Z", to: "2016-05-07T09:00:05.000Z" },
-  { from: "2016-05-07T09:00:00.000Z", to: "2016-05-07T09:15:07.000Z" },
-  { from: "2016-05-07T09:00:00.000Z", to: "2016-05-07T10:45:16.000Z" },
-  { from: "2016-05-07T09:00:00.000Z", to: "2016-05-07T13:59:03.000Z" },
-  { from: "2016-05-07T09:00:00.000Z", to: "2016-05-08T16:03:21.000Z" },
-  { from: "2016-05-07T09:00:00.000Z", to: "2016-05-21T01:22:12.000Z" },
-  { from: "2016-05-07T09:00:00.000Z", to: "2016-07-10T04:34:17.000Z" }
+  { from: "2016-05-07T09:00:00.000+10:00", to: "2016-05-07T09:00:00.000+10:00" },
+  { from: "2016-05-07T09:00:00.000+10:00", to: "2016-05-07T09:00:05.000+10:00" },
+  { from: "2016-05-07T09:00:00.000+10:00", to: "2016-05-07T09:15:07.000+10:00" },
+  { from: "2016-05-07T09:00:00.000+10:00", to: "2016-05-07T10:45:16.000+10:00" },
+  { from: "2016-05-07T09:00:00.000+10:00", to: "2016-05-07T13:59:03.000+10:00" },
+  { from: "2016-05-07T09:00:00.000+10:00", to: "2016-05-08T16:03:21.000+10:00" },
+  { from: "2016-05-07T09:00:00.000+10:00", to: "2016-05-21T01:22:12.000+10:00" },
+  { from: "2016-05-07T09:00:00.000+10:00", to: "2016-07-10T04:34:17.000+10:00" }
 ];
 
 describe('getDurationString', () => {
@@ -22,7 +22,7 @@ describe('getDurationString', () => {
   });
 
   it('defaults to full dates', () => {
-    expect(getDurationString("2016-10-05T03:39:57.000Z", "2016-10-05T03:40:02.000Z")).toMatchSnapshot();
+    expect(getDurationString("2016-10-05T03:39:57.000+10:00", "2016-10-05T03:40:02.000+10:00")).toMatchSnapshot();
   });
 
   getDurationString.formats.map((format) => {
@@ -34,13 +34,13 @@ describe('getDurationString', () => {
   });
 
   it('falls back to `now` when not supplied a `to` value', () => {
-    MockDate.set("2016-10-06T08:10:25.000Z");
-    expect(getDurationString("2016-10-05T03:40:02.000Z")).toMatchSnapshot();
-    expect(getDurationString("2016-10-05T03:40:02.000Z", undefined, "short")).toMatchSnapshot();
+    MockDate.set("2016-10-06T08:10:25.000+10:00");
+    expect(getDurationString("2016-10-05T03:40:02.000+10:00")).toMatchSnapshot();
+    expect(getDurationString("2016-10-05T03:40:02.000+10:00", undefined, "short")).toMatchSnapshot();
     MockDate.reset();
   });
 
   it('throws if supplied with an unknown format', () => {
-    expect(() => getDurationString("2016-10-05T03:40:02.000Z", "2016-10-05T03:40:02.000Z", "not-a-date-format")).toThrowErrorMatchingSnapshot();
+    expect(() => getDurationString("2016-10-05T03:40:02.000+10:00", "2016-10-05T03:40:02.000+10:00", "not-a-date-format")).toThrowErrorMatchingSnapshot();
   });
 });
