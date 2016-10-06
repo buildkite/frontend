41a0ab4e44d7baf5201172fc59c74c9e98603ba9
Jessica Stokes <hello@jessicastokes.net>
2016-10-06 17:25:25 +1100

    Add spec for friendlyRelativeTime

--- app/lib/friendlyRelativeTime.js
+++ app/lib/friendlyRelativeTime.js
@@ -23,16 +23,15 @@ function isThisYear(momentTime) {
 //   "Wed 13 Nov at 1:00 AM"
 //   "Fri 1 Jan 2012 at 4:02 PM"
 export default function friendlyRelativeTime(time, options = {}) {
-  const mTime = moment(time);
-  const timeString = options.seconds ? mTime.format('h:mm:s A') : mTime.format('h:mm A');
+  const momentTime = moment(time);
+  const timeString = momentTime.format(`h:mm${options.seconds ? ':ss' : ''} A`);
+  let dateString = momentTime.format(`ddd Do MMM${isThisYear(momentTime) ? '' : ' YY'}`);
 
-  if (isToday(mTime)) {
-    return `${ options.capitalized ? 'Today' : 'today' } at ${ timeString }`;
-  } else if (isYesterday(mTime)) {
-    return `${ options.capitalized ? 'Yesterday' : 'yesterday' } at ${ timeString }`;
-  } else if (isThisYear(mTime)) {
-    return `${ mTime.format('ddd Do MMM') } at ${ timeString }`;
-  } else {
-    return `${ mTime.format('ddd Do MMM YY') } at ${ timeString }`;
+  if (isToday(momentTime)) {
+    dateString = options.capitalized ? 'Today' : 'today';
+  } else if (isYesterday(momentTime)) {
+    dateString = options.capitalized ? 'Yesterday' : 'yesterday';
   }
+
+  return `${dateString} at ${timeString}`;
 }
---
+++ app/lib/friendlyRelativeTime.spec.js
@@ -0,0 +1,40 @@
+/* global describe, it, expect */
+import MockDate from 'mockdate';
+import friendlyRelativeTime from './friendlyRelativeTime';
+
+const DATE_FIXTURES = [
+  "2016-05-07T09:00:00.000Z",
+  "2016-05-07T16:45:16.000Z",
+  "2016-05-07T20:59:03.000Z",
+  "2016-05-08T16:03:21.000Z",
+  "2016-05-21T01:22:12.000Z",
+  "2016-07-10T04:34:17.000Z"
+];
+
+const BOOL_FIXTURES = [
+  undefined,
+  true,
+  false
+];
+
+describe('friendlyRelativeTime', () => {
+  BOOL_FIXTURES.forEach((seconds) => {
+    BOOL_FIXTURES.forEach((capitalized) => {
+      describe(`when supplied with \`{ seconds: ${seconds}, capitalized: ${capitalized} }\``, () => {
+        DATE_FIXTURES.forEach((date) => {
+          it(`renders a correct time in the past or present`, () => {
+            MockDate.set(date);
+            expect(friendlyRelativeTime(DATE_FIXTURES[0], {seconds, capitalized})).toMatchSnapshot();
+            MockDate.reset();
+          });
+
+          it(`renders a correct time in the future`, () => {
+            MockDate.set(DATE_FIXTURES[0]);
+            expect(friendlyRelativeTime(date, {seconds, capitalized})).toMatchSnapshot();
+            MockDate.reset();
+          });
+        });
+      });
+    });
+  });
+});
