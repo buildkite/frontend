b672d7e5e5577829749465382fde9ed6f1698e0b
Jessica Stokes <hello@jessicastokes.net>
2016-10-06 17:31:00 +1100

    Update friendlyRelativeTime spec to 100% coverage 💪🏼

--- app/lib/friendlyRelativeTime.spec.js
+++ app/lib/friendlyRelativeTime.spec.js
@@ -8,7 +8,8 @@ const DATE_FIXTURES = [
   "2016-05-07T20:59:03.000Z",
   "2016-05-08T16:03:21.000Z",
   "2016-05-21T01:22:12.000Z",
-  "2016-07-10T04:34:17.000Z"
+  "2016-07-10T04:34:17.000Z",
+  "2018-01-01T04:34:17.000Z"
 ];
 
 const BOOL_FIXTURES = [
@@ -17,24 +18,28 @@ const BOOL_FIXTURES = [
   false
 ];
 
+const runDateFixtureSpecs = (options) => (() => {
+  DATE_FIXTURES.forEach((date) => {
+    it(`renders a correct time in the past or present`, () => {
+      MockDate.set(date);
+      expect(friendlyRelativeTime(DATE_FIXTURES[0], options)).toMatchSnapshot();
+      MockDate.reset();
+    });
+
+    it(`renders a correct time in the future`, () => {
+      MockDate.set(DATE_FIXTURES[0]);
+      expect(friendlyRelativeTime(date, options)).toMatchSnapshot();
+      MockDate.reset();
+    });
+  });
+})
+
 describe('friendlyRelativeTime', () => {
   BOOL_FIXTURES.forEach((seconds) => {
     BOOL_FIXTURES.forEach((capitalized) => {
-      describe(`when supplied with \`{ seconds: ${seconds}, capitalized: ${capitalized} }\``, () => {
-        DATE_FIXTURES.forEach((date) => {
-          it(`renders a correct time in the past or present`, () => {
-            MockDate.set(date);
-            expect(friendlyRelativeTime(DATE_FIXTURES[0], {seconds, capitalized})).toMatchSnapshot();
-            MockDate.reset();
-          });
-
-          it(`renders a correct time in the future`, () => {
-            MockDate.set(DATE_FIXTURES[0]);
-            expect(friendlyRelativeTime(date, {seconds, capitalized})).toMatchSnapshot();
-            MockDate.reset();
-          });
-        });
-      });
+      describe(`when supplied with options=\`{ seconds: ${seconds}, capitalized: ${capitalized} }\``, runDateFixtureSpecs({seconds, capitalized}));
     });
   });
+
+  describe(`when supplied with options=undefined`, runDateFixtureSpecs());
 });
