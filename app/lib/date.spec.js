62201a7b5b7f434b418c2a6d04adca232bcfdf88
Jessica Stokes <hello@jessicastokes.net>
2016-10-06 15:27:33 +1100

    Use more codegen, fix rounding of durations

--- app/lib/date.spec.js
+++ app/lib/date.spec.js
@@ -16,14 +16,19 @@ describe('getDurationString', () => {
 
   getDurationString.formats.map((format) => {
     it(`correctly handles \`${format}\` dates`, () => {
-      expect(getDurationString("2016-10-05T03:39:57.000Z", "2016-10-05T03:40:02.000Z", format)).toMatchSnapshot();
-      expect(getDurationString("2016-09-15T06:49:27.000Z", "2016-09-15T06:52:55.000Z", format)).toMatchSnapshot();
-      expect(getDurationString("2016-09-15T06:49:27.000Z", "2016-09-15T08:52:55.000Z", format)).toMatchSnapshot();
+      expect(getDurationString("2016-05-07T09:00:00.000Z", "2016-05-07T09:00:00.000Z", format)).toMatchSnapshot();
+      expect(getDurationString("2016-05-07T09:00:00.000Z", "2016-05-07T09:00:05.000Z", format)).toMatchSnapshot();
+      expect(getDurationString("2016-05-07T09:00:00.000Z", "2016-05-07T09:15:07.000Z", format)).toMatchSnapshot();
+      expect(getDurationString("2016-05-07T09:00:00.000Z", "2016-05-07T10:45:16.000Z", format)).toMatchSnapshot();
+      expect(getDurationString("2016-05-07T09:00:00.000Z", "2016-05-07T13:59:03.000Z", format)).toMatchSnapshot();
+      expect(getDurationString("2016-05-07T09:00:00.000Z", "2016-05-08T16:03:21.000Z", format)).toMatchSnapshot();
+      expect(getDurationString("2016-05-07T09:00:00.000Z", "2016-05-21T01:22:12.000Z", format)).toMatchSnapshot();
+      expect(getDurationString("2016-05-07T09:00:00.000Z", "2016-07-10T04:34:17.000Z", format)).toMatchSnapshot();
     });
   });
 
   it('falls back to `now` when not supplied a `to` value', () => {
-    MockDate.set("2016-10-09T08:10:25.000Z");
+    MockDate.set("2016-10-06T08:10:25.000Z");
     expect(getDurationString("2016-10-05T03:40:02.000Z")).toMatchSnapshot();
     expect(getDurationString("2016-10-05T03:40:02.000Z", undefined, "short")).toMatchSnapshot();
     MockDate.reset();
