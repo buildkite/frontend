ef7e51454083a5fe7928f67e6e6b0c6c9b3a7d51
Jessica Stokes <hello@jessicastokes.net>
2016-10-14 18:20:19 +1100

    Add Buildkite slugs to docker environment

--- .buildkite/jest.sh
+++ .buildkite/jest.sh
@@ -7,4 +7,4 @@ npm run test-with-coverage --silent
 echo "👌 Looks good to me!"
 
 echo "+++ :compression: Creating coverage archive"
-tar -c -v -z -f "coverage/coverage-$BUILDKITE_ORGANIZATION_SLUG-$BUILDKITE_PIPELINE_SLUG-$BUILDKITE_BUILD_NUMBER.tgz" -C "coverage/lcov-report" "."
+tar -c -v -z -f "coverage/coverage-${BUILDKITE_ORGANIZATION_SLUG}-${BUILDKITE_PIPELINE_SLUG}-${BUILDKITE_BUILD_NUMBER}.tgz" -C "coverage/lcov-report" "."
