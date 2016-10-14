7472be10a54482e4b0a3ad6690fadf06e661879f
Jessica Stokes <hello@jessicastokes.net>
2016-10-14 18:16:33 +1100

    Make a tarball of the coverage report

--- .buildkite/jest.sh
+++ .buildkite/jest.sh
@@ -5,3 +5,6 @@ echo "+++ :jest: Running Jest"
 npm run test-with-coverage --silent
 
 echo "👌 Looks good to me!"
+
+echo "+++ :compression: Creating coverage archive"
+tar -c -v -z -f "coverage/coverage-$BUILDKITE_ORGANIZATION_SLUG-$BUILDKITE_PIPELINE_SLUG-$BUILDKITE_BUILD_NUMBER.tgz" -C "coverage/lcov-report" "."
