f0eaf1f3fa958036f2416fc70a9bd79c42ebe255
Jessica Stokes <hello@jessicastokes.net>
2016-10-14 18:29:01 +1100

    Use commit ID rather than build No.

--- .buildkite/jest.sh
+++ .buildkite/jest.sh
@@ -7,6 +7,6 @@ npm run test-with-coverage --silent
 echo "👌 Looks good to me!"
 
 echo "--- :compression: Packaging coverage report"
-tar -c -v -z -f "coverage/lcov-report-${BUILDKITE_ORGANIZATION_SLUG}-${BUILDKITE_PIPELINE_SLUG}-${BUILDKITE_BUILD_NUMBER}.tgz" -C "coverage/lcov-report" "."
+tar -c -v -z -f "coverage/lcov-report-${BUILDKITE_ORGANIZATION_SLUG}-${BUILDKITE_PIPELINE_SLUG}-${BUILDKITE_COMMIT}.tgz" -C "coverage/lcov-report" "."
 
 echo "📑 Done!"
