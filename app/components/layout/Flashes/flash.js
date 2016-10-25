201dd885b17eff7d5a2e17af60bab4e890ad1425
Samuel Cochran <sj26@sj26.com>
2016-10-25 16:11:10 +1100

    Better colours (thanks @toolmantim!)

--- app/components/layout/Flashes/flash.js
+++ app/components/layout/Flashes/flash.js
@@ -14,15 +14,15 @@ class Flash extends React.Component {
   };
 
   render() {
-    const classes = classNames("py2 px3 rounded flex items-center mb2 bg-gray", {
-      "bg-red white": this.props.flash.type === FlashesStore.ERROR,
-      "bg-green white": this.props.flash.type === FlashesStore.SUCCESS
+    const classes = classNames("flex items-center mb2 border rounded border-dark-gray", {
+      "border-red red": this.props.flash.type === FlashesStore.ERROR,
+      "border-lime lime": this.props.flash.type === FlashesStore.SUCCESS
     });
 
     return (
       <div key={this.props.flash.id} className={classes}>
-        <div className="flex-auto" data-flash-message={true}>{this.props.flash.message}</div>
-        <button onClick={this.handleCloseClick} className="btn px2 mxn2" data-flash-close={true}>Close</button>
+        <div className="flex-auto px4 py3" data-flash-message={true}>{this.props.flash.message}</div>
+        <button className="btn px4 py3" onClick={this.handleCloseClick} data-flash-close={true}>Close</button>
       </div>
     );
   }
