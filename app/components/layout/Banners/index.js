2c759dd22048b855bee5617ec7fc11edb99e94d9
Keith Pitt <me@keithpitt.com>
2016-10-25 14:08:38 +0800

    Hide banners when you click the "close" button

--- app/components/layout/Banners/index.js
+++ app/components/layout/Banners/index.js
@@ -3,16 +3,36 @@ import React from 'react';
 import PipelinesBanner from './pipelines';
 
 class Banners extends React.Component {
+  state = {
+    hidden: {}
+  };
+
   render() {
-    return (
-      <div className="container mb4">
-        <PipelinesBanner id="pipelines" onHideClick={this.handleBannerHide} />
-      </div>
-    )
+    let nodes = [];
+    if(window._banners) {
+      for(const id of window._banners) {
+        if(!this.state.hidden[id]) {
+          nodes.push(<PipelinesBanner id={id} key={id} onHideClick={this.handleBannerHide} />);
+        }
+      }
+    }
+
+    if(nodes.length) {
+      return (
+        <div className="container mb4">
+          {nodes}
+        </div>
+      )
+    } else {
+      return null;
+    }
   }
 
   handleBannerHide = (id) => {
-    console.log('removing ', id);
+    let hidden = this.state.hidden;
+    hidden[id] = true;
+
+    this.setState({ hidden: hidden });
   };
 }
 
