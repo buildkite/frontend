fd3be889a062f67b4f092b3a8b743051f7386d22
Jessica Stokes <hello@jessicastokes.net>
2016-10-27 14:44:33 +0800

    Add specs for Emojify

---
+++ app/components/shared/Emojify.spec.js
@@ -0,0 +1,27 @@
+/* global describe, it, jest, expect */
+import React from 'react';
+import ReactTestRenderer from 'react-test-renderer';
+
+const FIXTURE_TEXT = "This text should be reflected in the title attribute and text content, as well as in the call to lib/Emoji!";
+
+describe('Emojify', () => {
+  it('renders as expected', () => {
+    const parse = jest.fn((text) => text);
+
+    jest.mock('../../lib/Emoji', () => ({ parse }));
+
+    const { default: Emojify } = require('./Emojify');
+
+    const component = ReactTestRenderer.create(
+      <Emojify
+        text="This text should be reflected in the title attribute and text content, as well as in the call to lib/Emoji!"
+        className="test-class-name"
+        updateFrequency={0}
+      />
+    );
+
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+    expect(parse).toHaveBeenCalledWith(FIXTURE_TEXT);
+  });
+});
