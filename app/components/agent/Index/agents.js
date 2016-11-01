2228b5bc6dfad82921fe8a67b4a38065039ac9c2
Keith Pitt <me@keithpitt.com>
2016-11-01 12:35:20 +1100

    Extracted the agents list into it's own component + added deferred loading

---
+++ app/components/agent/Index/agents.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import Relay from 'react-relay';
+
+import Panel from '../../shared/Panel';
+import Button from '../../shared/Button';
+import Spinner from '../../shared/Spinner';
+
+import AgentRow from './row';
+
+const AGENT_LIST_REFRESH_INTERVAL = 10 * 1000;
+const PAGE_SIZE = 20;
+
+class Agents extends React.Component {
+  static propTypes = {
+    organization: React.PropTypes.shape({
+      agents: React.PropTypes.shape({
+        pageInfo: React.PropTypes.shape({
+          hasNextPage: React.PropTypes.bool.isRequired
+        }).isRequired,
+        edges: React.PropTypes.array.isRequired
+      })
+    }).isRequired,
+    relay: React.PropTypes.object.isRequired
+  };
+
+  componentDidMount() {
+    this._agentListRefreshInterval = setInterval(this.fetchUpdatedData, AGENT_LIST_REFRESH_INTERVAL);
+    this.props.relay.setVariables({ isMounted: true });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this._agentListRefreshInterval);
+  }
+
+  fetchUpdatedData = () => {
+    this.props.relay.forceFetch(true);
+  };
+
+  render() {
+    return (
+      <Panel>
+        <Panel.Header>Agents {this.renderHeaderInformation()}</Panel.Header>
+        {this.renderRows()}
+        {this.renderLoadMoreButton()}
+      </Panel>
+    )
+  }
+
+  renderHeaderInformation() {
+    if(this.props.organization.agents) {
+      return (
+        <span>(Showing {this.props.organization.agents.edges.length} of {this.props.organization.agents.count})</span>
+      )
+    }
+  }
+
+  renderRows() {
+    let agents = this.props.organization.agents;
+
+    if(agents) {
+      if (agents.edges.length > 0) {
+        return agents.edges.map((edge) => <AgentRow key={edge.node.id} agent={edge.node} />);
+      } else {
+        return <Panel.Section className="dark-gray">No agents connected</Panel.Section>;
+      }
+    } else {
+      return (
+        <Panel.Section className="center">
+          <Spinner />
+        </Panel.Section>
+      )
+    }
+  }
+
+  renderLoadMoreButton() {
+    if(this.props.organization.agents) {
+      if (this.props.organization.agents.pageInfo.hasNextPage) {
+        return (
+          <Panel.Footer>
+            <Button outline={true} theme={"default"} onClick={this.handleLoadMoreAgentsClick}>Load {PAGE_SIZE} more agents...</Button>
+          </Panel.Footer>
+        );
+      } else {
+        return (
+          <Panel.Footer>
+            <small className="dark-gray">No more to load</small>
+          </Panel.Footer>
+        );
+      }
+    }
+  }
+
+  handleLoadMoreAgentsClick = () => {
+    this.props.relay.setVariables({
+      pageSize: this.props.relay.variables.pageSize + PAGE_SIZE
+    });
+  };
+}
+
+export default Relay.createContainer(Agents, {
+  initialVariables: {
+    isMounted: false,
+    pageSize: PAGE_SIZE
+  },
+
+  fragments: {
+    organization: () => Relay.QL`
+      fragment on Organization {
+        agents(first: $pageSize) @include(if: $isMounted) {
+          count
+          edges {
+            node {
+              id
+              ${AgentRow.getFragment('agent')}
+            }
+          }
+          pageInfo {
+            hasNextPage
+          }
+        }
+      }
+    `
+  }
+});
--- app/components/agent/Index/index.js
+++ app/components/agent/Index/index.js
@@ -1,29 +1,16 @@
 import React from 'react';
 import Relay from 'react-relay';
 import DocumentTitle from 'react-document-title';
-import classNames from 'classnames';
 
-import AgentRow from './row';
-import AgentTokens from './agentTokens';
-
-import Panel from '../../shared/Panel';
 import PageWithContainer from '../../shared/PageWithContainer';
-import Button from '../../shared/Button';
 
-const AGENT_LIST_REFRESH_INTERVAL = 10 * 1000;
-const PAGE_SIZE = 20;
+import Agents from './agents';
+import AgentTokens from './agentTokens';
 
 class AgentIndex extends React.Component {
   static propTypes = {
     organization: React.PropTypes.shape({
       name: React.PropTypes.string.isRequired,
-      agents: React.PropTypes.shape({
-        pageInfo: React.PropTypes.shape({
-          hasNextPage: React.PropTypes.bool.isRequired
-        }).isRequired,
-        count: React.PropTypes.number.isRequired,
-        edges: React.PropTypes.array.isRequired
-      }).isRequired,
       permissions: React.PropTypes.shape({
         agentTokenView: React.PropTypes.shape({
           allowed: React.PropTypes.bool.isRequired
@@ -33,101 +20,48 @@ class AgentIndex extends React.Component {
     relay: React.PropTypes.object.isRequired
   };
 
-  componentDidMount() {
-    this._agentListRefreshInterval = setInterval(this.fetchUpdatedData, AGENT_LIST_REFRESH_INTERVAL);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this._agentListRefreshInterval);
-  }
-
-  fetchUpdatedData = () => {
-    this.props.relay.forceFetch(true);
-  };
-
   render() {
-    const tokenViewAllowed = this.props.organization.permissions.agentTokenView.allowed;
-
-    let loadMoreButton;
-    if (this.props.organization.agents.pageInfo.hasNextPage) {
-      loadMoreButton =
-        <Button outline={true} theme={"default"} onClick={this.handleLoadMoreAgentsClick}>Load {PAGE_SIZE} more agents...</Button>;
-    } else {
-      loadMoreButton = <small className="dark-gray">No more to load</small>;
-    }
-
-    let pageContent = (
-      <Panel>
-        <Panel.Header>Agents (Showing {this.props.organization.agents.edges.length} of {this.props.organization.agents.count})</Panel.Header>
-        {this.renderAgentList(this.props.organization.agents)}
-        <Panel.Footer>
-          {loadMoreButton}
-        </Panel.Footer>
-      </Panel>
+    return (
+      <DocumentTitle title={`Agents · ${this.props.organization.name}`}>
+        <PageWithContainer>
+          {this.renderContent()}
+        </PageWithContainer>
+      </DocumentTitle>
     );
+  }
 
-    if (tokenViewAllowed) {
-      pageContent = (
+  renderContent() {
+    // Switches between showing just the agents, or the agents along with
+    // registration tokens.
+    if (this.props.organization.permissions.agentTokenView.allowed) {
+      return (
         <div className="clearfix mxn3">
           <div className="sm-col sm-col-8 px3">
-            {pageContent}
+            <Agents organization={this.props.organization} />
           </div>
           <div className="sm-col sm-col-4 px3">
             <AgentTokens organization={this.props.organization} />
           </div>
         </div>
       );
-    }
-
-    return (
-      <DocumentTitle title={`Agents · ${this.props.organization.name}`}>
-        <PageWithContainer>
-          {pageContent}
-        </PageWithContainer>
-      </DocumentTitle>
-    );
-  }
-
-  renderAgentList(agents) {
-    if (agents.edges.length > 0) {
-      return agents.edges.map((edge) => <AgentRow key={edge.node.id} agent={edge.node} />);
     } else {
-      return <Panel.Section className="dark-gray">No agents connected</Panel.Section>;
+      return (
+        <Agents organization={this.props.organization} />
+      )
     }
   }
-
-  handleLoadMoreAgentsClick = () => {
-    this.props.relay.setVariables({
-      pageSize: this.props.relay.variables.pageSize + PAGE_SIZE
-    });
-  };
 }
 
 export default Relay.createContainer(AgentIndex, {
-  initialVariables: {
-    pageSize: PAGE_SIZE
-  },
-
   fragments: {
     organization: () => Relay.QL`
       fragment on Organization {
         ${AgentTokens.getFragment('organization')}
+        ${Agents.getFragment('organization')}
         name
         permissions {
           agentTokenView {
             allowed
-          }
-        }
-        agents(first: $pageSize) {
-          count
-          edges {
-            node {
-              id
-              ${AgentRow.getFragment('agent')}
-            }
-          }
-          pageInfo {
-            hasNextPage
           }
         }
       }
