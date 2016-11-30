80d46c6afdee6e58bb905e0fe8aacf0c63e691be
Jessica Stokes <hello@jessicastokes.net>
2016-11-30 16:29:06 +1100

    Fix ESLint error

--- app/components/icons/Favorite.js
+++ app/components/icons/Favorite.js
@@ -25,7 +25,7 @@ class Favorite extends React.Component {
 
   render() {
     return (
-      <StarSVG width="20px" height="15px" viewBox="0 0 16 15" {...this.props}>
+      <StarSVG width="20px" height="15px" viewBox="0 0 16 15" favorite={this.props.favorite}>
         <title>Favorite</title>
         <g className="star" transform="translate(-1103, -19)">
           <polygon points="1111 31 1106.29772 33.472136 1107.19577 28.236068 1103.39155 24.527864 1108.64886 23.763932 1111 19 1113.35114 23.763932 1118.60845 24.527864 1114.80423 28.236068 1115.70228 33.472136" />
