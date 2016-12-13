99b7ad65d3999c16a05f6afb5679d4c814b4e4b9
Jessica Stokes <hello@jessicastokes.net>
2016-12-13 14:43:11 -0800

    Add EmailCreate mutation definition

---
+++ app/mutations/EmailCreate.js
@@ -0,0 +1,57 @@
+import Relay from 'react-relay';
+
+class EmailCreate extends Relay.Mutation {
+  getMutation() {
+    return Relay.QL`
+      mutation {
+        emailCreate
+      }
+    `;
+  }
+
+  getFatQuery() {
+    return Relay.QL`
+      fragment on EmailCreatePayload {
+        emailEdge,
+        viewer {
+          emails {
+            count
+          }
+        }
+      }
+    `;
+  }
+
+  getConfigs() {
+    return [{
+      type: 'REQUIRED_CHILDREN',
+      children: [
+        Relay.QL`
+          fragment on EmailCreatePayload {
+            emailEdge {
+              node {
+                address
+                primary
+                verified
+              }
+            }
+          }
+        `
+      ]
+    }, {
+      type: 'RANGE_ADD',
+      parentName: 'viewer',
+      connectionName: 'emails',
+      edgeName: 'emailEdge',
+      rangeBehaviors: {
+        '': 'append'
+      }
+    }];
+  }
+
+  getVariables() {
+    return { address: this.props.address };
+  }
+}
+
+export default EmailCreate;
