b5563afef9d674db88d41c9db7425ce648c4644e
Jessica Stokes <hello@jessicastokes.net>
2017-01-30 16:09:12 -0800

    Make the Changelogs badge bulletproof
    Previously, it suffered from the same state caching issues as other Pusher-powered things that could also be interacted with (opening, for instance, the Organizations dropdown, would cause it to use the cached state), this updates it to act correctly in all cases.
    
    It also makes it a relay container which accepts the isMounted variable from its parent, so that it can force an update of only its data set whilst also being updated on-mount without triggering a separate request for its data!

--- app/components/user/NewChangelogsBadge.js
+++ app/components/user/NewChangelogsBadge.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Relay from 'react-relay';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import shallowCompare from 'react-addons-shallow-compare';
 import classNames from 'classnames';
 
 import PusherStore from '../../stores/PusherStore';
@@ -15,10 +17,6 @@ class NewChangelogsBadge extends React.Component {
     })
   };
 
-  state = {
-    unreadChangelogsCount: this.props.viewer.unreadChangelogs ? this.props.viewer.unreadChangelogs.count : 0
-  };
-
   componentDidMount() {
     PusherStore.on("user_stats:change", this.handlePusherWebsocketEvent);
   }
@@ -28,41 +26,71 @@ class NewChangelogsBadge extends React.Component {
   }
 
   handlePusherWebsocketEvent = (payload) => {
-    this.setState({ unreadChangelogsCount: payload.unreadChangelogsCount });
+    this.props.relay.forceFetch();
   };
 
-  componentWillReceiveProps = (nextProps) => {
-    if (nextProps.viewer.unreadChangelogs) {
-      this.setState({ unreadChangelogsCount: nextProps.viewer.unreadChangelogs.count });
-    }
-  }
-
   shouldComponentUpdate = (nextProps, nextState) => {
-    const { unreadChangeLogsCount: lastChangeLogsCount } = this.state;
-
-    const { unreadChangeLogsCount: newChangeLogsCount } = nextState;
-    const { unreadChangelogs: newUnreadChangelogs } = nextProps.viewer;
-
-    return (
-      (newUnreadChangelogs && newUnreadChangelogs.count !== lastChangeLogsCount)
-      || (newChangeLogsCount !== lastChangeLogsCount)
-    );
+    return shallowCompare(this, nextProps, nextState);
   };
 
   render() {
-    if (this.state.unreadChangelogsCount > 0) {
-      return (
-        <ReactCSSTransitionGroup transitionName="transition-changelog-appear" transitionAppear={true} transitionAppearTimeout={3000} transitionEnterTimeout={5000} transitionLeaveTimeout={5000}>
-          <span key="badge" className={classNames(this.props.className, "inline-block hover-faded")} style={this.props.style}>
-            <a href="/changelog" className="btn bg-lime white hover-white focus-white line-height-1" style={{ padding: '3px 4px', fontSize: '9px', borderRadius: 5 }}>NEW</a>
-            <img className="pointer-events-none" src={require('../../images/new-changelog-badge-bottom-corner.svg')} style={{ width: 12, height: 12, position: 'absolute', left: -3, bottom: -2 }} />
-          </span>
-        </ReactCSSTransitionGroup>
-      );
-    } else {
+    if (!this.props.viewer.unreadChangelogs || !this.props.viewer.unreadChangelogs.count) {
       return null;
     }
+
+    return (
+      <ReactCSSTransitionGroup
+        transitionName="transition-changelog-appear"
+        transitionAppear={true}
+        transitionAppearTimeout={3000}
+        transitionEnterTimeout={5000}
+        transitionLeaveTimeout={5000}
+      >
+        <span
+          key="badge"
+          className={classNames(this.props.className, "inline-block hover-faded")}
+          style={this.props.style}
+        >
+          <a
+            href="/changelog"
+            className="btn bg-lime white hover-white focus-white line-height-1"
+            style={{
+              padding: '3px 4px',
+              fontSize: 9,
+              borderRadius: 5
+            }}
+          >
+            NEW
+          </a>
+          <img
+            className="pointer-events-none"
+            src={require('../../images/new-changelog-badge-bottom-corner.svg')}
+            style={{
+              width: 12,
+              height: 12,
+              position: 'absolute',
+              left: -3,
+              bottom: -2
+            }}
+          />
+        </span>
+      </ReactCSSTransitionGroup>
+    );
   }
 }
 
-export default NewChangelogsBadge;
+export default Relay.createContainer(NewChangelogsBadge, {
+  initialVariables: {
+    isMounted: false
+  },
+
+  fragments: {
+    viewer: () => Relay.QL`
+        fragment on Viewer {
+          unreadChangelogs: changelogs(read: false) @include(if: $isMounted) {
+            count
+          }
+        }
+      `
+  }
+});
