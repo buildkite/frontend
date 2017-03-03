7e6cc591dbc306f78b0d6b025e69cd9a4ebfc4dd
Jessica Stokes <hello@jessicastokes.net>
2017-03-03 13:08:30 +1100

    Make organizationMemberSlug unique, add warning

--- app/queries/OrganizationMember.js
+++ app/queries/OrganizationMember.js
@@ -1,14 +1,18 @@
 import Relay from 'react-relay';
 
+// Note: Ensure whatever variable you use as the $slug is unique and doesn't
+// match any existing variables in any other queries otherwise React Relay
+// Router doesn't work very well.
+
 export const query = () => Relay.QL`
   query {
-    organizationMember(slug: $slug)
+    organizationMember(slug: $organizationMemberSlug)
   }
 `;
 
 export const prepareParams = (params) => {
   return {
     ...params,
-    slug: [params.organization, params.organizationMember].join("/")
+    organizationMemberSlug: [params.organization, params.organizationMember].join("/")
   };
 };
