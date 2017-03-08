b606e460cb1536290e0be17b00a4552070416b0a
Jessica Stokes <hello@jessicastokes.net>
2017-03-08 13:04:39 +1100

    Add OrganizationInvitationCreate mutation

---
+++ app/mutations/OrganizationInvitationCreate.js
@@ -0,0 +1,61 @@
+import Relay from 'react-relay';
+
+import OrganizationMemberRoleConstants from '../constants/OrganizationMemberRoleConstants';
+
+export default class OrganizationInvitationCreate extends Relay.Mutation {
+  static fragments = {
+    organizationMember: () => Relay.QL`
+      fragment on OrganizationInvitation {
+        id
+      }
+    `
+  }
+
+  getMutation() {
+    return Relay.QL`
+      mutation {
+        organizationInvitationCreate
+      }
+    `;
+  }
+
+  getFatQuery() {
+    return Relay.QL`
+      fragment on OrganizationInvitationCreatePayload {
+        invitationEdges {
+          node {
+            role
+            user {
+              name
+            }
+          }
+        }
+      }
+    `;
+  }
+
+  getOptimisticResponse() {
+    return {
+      organizationInvitationEdge: {
+        node: {
+          role: OrganizationMemberRoleConstants.MEMBER
+        }
+      }
+    };
+  }
+
+  getConfigs() {
+    return [{
+      type: 'RANGE_ADD',
+      parentName: 'organization',
+      parentID: this.props.organization.id,
+      connectionName: 'invitations',
+      edgeName: 'organizationInvitationEdge',
+      rangeBehaviors: () => 'prepend'
+    }];
+  }
+
+  getVariables() {
+    return { id: this.props.organizationMember.id, role: this.props.role };
+  }
+}
