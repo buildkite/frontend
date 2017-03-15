8f3f2e115c69065e613cd63223e9d8701530d4f9
Keith Pitt <me@keithpitt.com>
2017-03-15 15:13:26 +1100

    Added a state switcher to the jobs explorer

--- app/components/job/Index/index.js
+++ app/components/job/Index/index.js
@@ -3,15 +3,24 @@ import Relay from 'react-relay';
 
 import Spinner from '../../shared/Spinner';
 import Panel from '../../shared/Panel';
+import Button from '../../shared/Button';
+import Icon from '../../shared/Icon';
 import PageWithContainer from '../../shared/PageWithContainer';
 
 import JobRow from './job-row';
+import StateSelector from './state-selector';
 
 const PAGE_SIZE = 100;
 
 class AgentIndex extends React.Component {
   static propTypes = {
     organization: React.PropTypes.object.isRequired
+  };
+
+  state = {
+    loadingMore: false,
+    switchingStates: false,
+    selectedJobState: null
   };
 
   componentDidMount() {
@@ -27,34 +36,115 @@ class AgentIndex extends React.Component {
       <PageWithContainer>
         <Panel>
           <Panel.Header>Job Explorer</Panel.Header>
+          <Panel.Section>
+            <div className="flex items-center">
+              <div className="relative flex-auto">
+                <div className="absolute pointer-events-none" style={{ left: 8, top: 5 }}>
+                  {this.renderSearchIcon()}
+                </div>
+                <input type="text" className="input" placeholder="Search by agent query rules…" style={{ paddingLeft: 28 }} />
+              </div>
+
+              <div className="flex-none pl3 flex">
+                <div className="semi-bold mr1">States:</div> <StateSelector selection={this.state.selectedJobState} onSelect={this.handleStateSelection} />
+              </div>
+            </div>
+          </Panel.Section>
           {this.renderJobsList()}
+          {this.renderFooter()}
         </Panel>
       </PageWithContainer>
     );
   }
 
+  renderSearchIcon() {
+    if (this.state.searching) {
+      return (
+        <Spinner size={15} color={false}/>
+      );
+    } else {
+      return (
+        <Icon icon="search" className="gray" style={{ width: 15, height: 15 }} />
+      );
+    }
+  }
+
+  renderFooter() {
+    if(!this.props.organization.jobs || !this.props.organization.jobs.pageInfo.hasNextPage || this.state.switchingStates) {
+      return null;
+    }
+
+    if(this.state.loadingMore) {
+      return (
+        <Panel.Footer className="center">
+          <Spinner style={{ margin: 8 }} />
+        </Panel.Footer>
+      );
+    } else {
+      return (
+        <Panel.Footer className="center">
+          <Button outline={true} theme="default" onClick={this.handleLoadMoreClick}>Load more…</Button>
+        </Panel.Footer>
+      )
+    }
+  }
+
   renderJobsList() {
     const jobs = this.props.organization.jobs;
 
-    if(jobs) {
-      return jobs.edges.map((edge) => {
-        return (
-          <JobRow key={edge.node.id} job={edge.node} />
-        );
-      });
-    } else {
+    if(!jobs || this.state.switchingStates) {
       return (
         <Panel.Section className="center">
           <Spinner />
         </Panel.Section>
       )
+    } else {
+      if (jobs.edges.length == 0) {
+        return (
+          <Panel.Section className="center">
+            <div>No jobs here!</div>
+          </Panel.Section>
+        )
+      } else {
+        return jobs.edges.map((edge) => {
+          return (
+            <JobRow key={edge.node.id} job={edge.node} />
+          );
+        });
+      }
     }
   }
+
+  handleStateSelection = (state) => {
+    this.setState({ switchingStates: true, selectedJobState: state });
+
+    // Dunno why state comes through as `null`
+    const newState = state == "null" ? null : state;
+
+    this.props.relay.forceFetch({ state: newState, pageSize: PAGE_SIZE }, (readyState) => {
+      if (readyState.done) {
+        this.setState({ switchingStates: false });
+      }
+    });
+  };
+
+  handleLoadMoreClick = () => {
+    this.setState({ loadingMore: true });
+
+    const newPageSize = this.props.relay.variables.pageSize + PAGE_SIZE;
+
+    this.props.relay.setVariables({ pageSize: newPageSize }, (readyState) => {
+      if (readyState.done) {
+        this.setState({ loadingMore: false });
+      }
+    });
+  };
 }
 
 export default Relay.createContainer(AgentIndex, {
   initialVariables: {
     isMounted: false,
+    state: null,
     agentQueryRules: null,
     pageSize: PAGE_SIZE
   },
@@ -62,7 +152,8 @@ export default Relay.createContainer(AgentIndex, {
   fragments: {
     organization: () => Relay.QL`
       fragment on Organization {
-        jobs(first: $pageSize, type: COMMAND) @include(if: $isMounted) {
+        jobs(first: $pageSize, type: COMMAND, state: $state) @include(if: $isMounted) {
+          count
           edges {
             node {
               ...on JobTypeCommand {
@@ -70,6 +161,9 @@ export default Relay.createContainer(AgentIndex, {
               }
               ${JobRow.getFragment('job')}
             }
+          }
+          pageInfo {
+            hasNextPage
           }
         }
       }
--- app/components/job/Index/job-row.js
+++ app/components/job/Index/job-row.js
@@ -2,14 +2,40 @@ import React from 'react';
 import Relay from 'react-relay';
 
 import Panel from '../../shared/Panel';
+import Emojify from '../../shared/Emojify';
+import JobLink from '../../shared/JobLink';
+import FriendlyTime from '../../shared/FriendlyTime';
 
 class JobRow extends React.Component {
   render() {
     return (
       <Panel.Row>
-        <div>{this.props.job.label}</div>
+        <div className="flex items-center">
+          <div className="flex-none" style={{ width: 120 }}>
+            {this.props.job.state}
+          </div>
+          <div className="flex-auto">
+            <JobLink job={this.props.job} />
+            <div className="dark-gray mt1">{this.renderQueryRules()}</div>
+          </div>
+          <div className="flex-none dark-gray">
+            Created <FriendlyTime value={this.props.job.createdAt} />
+          </div>
+        </div>
       </Panel.Row>
     );
+  }
+
+  renderQueryRules() {
+    if(!this.props.job.agentQueryRules.length) {
+      return (
+        <code>queue=default</code>
+      )
+    } else {
+      return (
+        <code>{this.props.job.agentQueryRules.join(", ")}</code>
+      )
+    }
   }
 }
 
@@ -18,14 +44,12 @@ export default Relay.createContainer(JobRow, {
     job: () => Relay.QL`
       fragment on Job {
         ...on JobTypeCommand {
-          pipeline {
-            name
-            slug
-          }
+          id
           state
-          label
           agentQueryRules
+          createdAt
         }
+        ${JobLink.getFragment('job')}
       }
     `
   }
