b1a99c6d7816e694b34855199a2ee3b38436e1f1
Keith Pitt <me@keithpitt.com>
2017-04-10 12:53:15 +1000

    Added some words around SSO

--- app/components/sso/Index.js
+++ app/components/sso/Index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Relay from 'react-relay';
 import DocumentTitle from 'react-document-title';
 
+import Panel from '../shared/Panel';
 import PageHeader from '../shared/PageHeader';
+import Spinner from '../shared/Spinner';
 
 import permissions from '../../lib/permissions';
 
@@ -16,6 +18,10 @@ class SSOIndex extends React.PureComponent {
     relay: React.PropTypes.object.isRequired
   };
 
+  componentDidMount() {
+    this.props.relay.forceFetch({ isMounted: true });
+  }
+
   render() {
     return (
       <DocumentTitle title={`SSO · ${this.props.organization.name}`}>
@@ -25,21 +31,71 @@ class SSOIndex extends React.PureComponent {
               Single Sign On
             </PageHeader.Title>
             <PageHeader.Description>
-              SSO enables you to automatically onboard users without having to manually invite them. SSO is available via SAML (Okta, Bitium, etc) or with GSuite (formally Google Apps).
+              SSO enables you to automatically onboard users without having to manually invite them via email. SSO is available via SAML (Okta, Bitium, etc) or Google Apps (GSuite).
             </PageHeader.Description>
           </PageHeader>
+
+          {this.renderDetailsPanel()}
         </div>
       </DocumentTitle>
     );
   }
+
+  renderDetailsPanel() {
+    if(!this.props.organization.sso) {
+      return (
+        <Panel>
+          <Panel.Section className="center">
+            <Spinner />
+          </Panel.Section>
+        </Panel>
+      );
+    }
+
+    if(this.props.organization.sso.isEnabled) {
+      return (
+        <Panel>
+          <Panel.Section>
+            <p><span className="green">✔</span> SSO has been enabled for your organization via <strong>{this.props.organization.sso.provider.name}</strong>. Users will be automatically added to your organization when they successfully authenticate using {this.props.organization.sso.provider.name} and their <strong>{this.props.organization.sso.provider.emailDomain}</strong> email address.</p>
+            <p>If you need to update your SSO settings or have it disabled, please get in touch with Buildkite Support.</p>
+          </Panel.Section>
+        </Panel>
+      )
+    } else {
+      return (
+        <Panel>
+          <Panel.Section>
+            <p>SSO has not yet been enabled for your organization.</p>
+            <p>If you want to find out more about SSO or have it enabled, please get in touch with Buildkite Support.</p>
+          </Panel.Section>
+        </Panel>
+      )
+    }
+  }
 }
 
 export default Relay.createContainer(SSOIndex, {
+  initialVariables: {
+    isMounted: false
+  },
+
   fragments: {
     organization: () => Relay.QL`
       fragment on Organization {
         name
         slug
+        sso @include(if: $isMounted) {
+          isEnabled
+          provider {
+            name
+            ...on SSOProviderGoogle {
+              emailDomain
+            }
+            ...on SSOProviderSAML {
+              emailDomain
+            }
+          }
+        }
         permissions {
           organizationUpdate {
             allowed
