df947342f29a86df9b180e42003581a417a84b12
Jessica Stokes <hello@jessicastokes.net>
2017-05-11 11:54:37 -0700

    Emit all Pusher state changes from PusherStore
    This will allow us to handle receipt these events more gracefully in future!

--- app/stores/PusherStore.js
+++ app/stores/PusherStore.js
@@ -6,7 +6,14 @@ class PusherStore extends EventEmitter {
   configure(key, options) {
     this.pusher = new Pusher(key, options);
 
-    this.pusher.connection.bind("connected", () => this.emit("connected"));
+    this.pusher.connection.bind(
+      'state_change',
+      ({ current, ...context }) => {
+        Logger.info("[PusherStore]", current, context);
+
+        this.emit(current, context);
+      }
+    );
   }
 
   isConfigured() {
