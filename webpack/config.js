5ac1514dc27ffc8906020f73d18f99fc80f123e6
Keith Pitt <me@keithpitt.com>
2017-05-31 16:13:55 +1000

    Remove `relay-compiler-webpack-plugin` since it seems a little buggy

--- webpack/config.js
+++ webpack/config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const webpack = require("webpack");
 const AssetsPlugin = require('assets-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-// const RelayCompilerPlugin = require("relay-compiler-webpack-plugin");
 
 // Ensure a FRONTEND_HOST is setup since we embed it in the assets.json file
 if (!process.env.FRONTEND_HOST) {
@@ -84,13 +83,7 @@ var plugins = [
     }
   }),
 
-  new ExtractTextPlugin("[name]-[contenthash].css"),
-
-  // Automatically compile GraphQL fragments for Relay
-  // new RelayCompilerPlugin({
-  //   schema: path.resolve(__dirname, "../app/graph/schema.json"),
-  //   src: path.resolve(__dirname, "../app")
-  // })
+  new ExtractTextPlugin("[name]-[contenthash].css")
 ];
 
 var vendor_modules = [
