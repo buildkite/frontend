1dd7e4fab2a820ae76c845dabd41383a3255dd68
Jessica Stokes <hello@jessicastokes.net>
2017-06-06 11:28:37 -0700

    Improve Emojify's output and consistency

--- app/components/shared/Emojify.js
+++ app/components/shared/Emojify.js
@@ -3,25 +3,35 @@ import PropTypes from 'prop-types';
 
 import Emoji from '../../lib/Emoji';
 
-class Emojify extends React.PureComponent {
+export default class Emojify extends React.PureComponent {
   static propTypes = {
     text: PropTypes.string,
+    title: PropTypes.string,
     className: PropTypes.string,
     style: PropTypes.object
   };
 
   render() {
-    const { className, text, style } = this.props;
+    const spanProps = {};
+    const html = Emoji.parse(this.props.text);
+
+    if (html !== this.props.text) {
+      spanProps.dangerouslySetInnerHTML = { __html: html };
+      spanProps.title = this.props.text;
+    } else {
+      spanProps.children = this.props.text;
+    }
+
+    if (this.props.title) {
+      spanProps.title = this.props.title;
+    }
 
     return (
       <span
-        className={className}
-        style={style}
-        title={text}
-        dangerouslySetInnerHTML={{ __html: Emoji.parse(text) }}
+        className={this.props.className}
+        style={this.props.style}
+        {...spanProps}
       />
     );
   }
 }
-
-export default Emojify;
