55f5f6646e52d56c6bb87e7fbac686673965c9b4
Tim Lucas <t@toolmantim.com>
2017-07-10 17:30:25 +1000

    New membership permission descriptions and icons

--- app/components/team/Members/role.js
+++ app/components/team/Members/role.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import Chooser from '../../shared/Chooser';
 import Dropdown from '../../shared/Dropdown';
+import Icon from '../../shared/Icon';
 
 import TeamMemberRoleConstants from '../../../constants/TeamMemberRoleConstants';
 
@@ -20,6 +21,27 @@ export default class MemberRole extends React.PureComponent {
   render() {
     const saving = this.props.savingNewRole;
 
+    const PermissionBlock = ({ children }) => (
+      <div className="pointer-events-none" style={{ marginTop: 8, marginLeft: -4 }}>
+        {children}
+      </div>
+    );
+
+    const Permission = ({ icon, children }) => (
+      <div className="flex mt1" style={{ lineHeight: 1.4 }}>
+        <Icon icon={icon} className="dark-gray flex-none mr1" style={{ marginTop: -3 }} />
+        {children}
+      </div>
+    )
+
+    const Can = ({ permission }) => (
+      <Permission icon="permission-small-tick" title="Tick">Can {permission}.</Permission>
+    );
+
+    const CanNot = ({ permission }) => (
+      <Permission icon="permission-small-cross" title="Cross">Can not {permission}.</Permission>
+    );
+
     return (
       <Dropdown width={270}>
         <div className="underline-dotted cursor-pointer inline-block regular">{this.label(this.props.teamMember.role)}</div>
@@ -30,14 +52,24 @@ export default class MemberRole extends React.PureComponent {
             saving={saving === TeamMemberRoleConstants.MAINTAINER}
             selected={this.props.teamMember.role === TeamMemberRoleConstants.MAINTAINER}
             label={this.label(TeamMemberRoleConstants.MAINTAINER)}
-            description={<span>Can create and access pipelines.<br />Can add and remove users.</span>}
+            description={
+              <PermissionBlock>
+                <Can permission="add and remove pipelines" />
+                <Can permission="add and remove users" />
+              </PermissionBlock>
+            }
           />
           <Chooser.SelectOption
             value={TeamMemberRoleConstants.MEMBER}
             saving={saving === TeamMemberRoleConstants.MEMBER}
             selected={this.props.teamMember.role === TeamMemberRoleConstants.MEMBER}
             label={this.label(TeamMemberRoleConstants.MEMBER)}
-            description="Can create and access pipelines."
+            description={
+              <PermissionBlock>
+                <Can permission="and and remove pipelines" />
+                <CanNot permission="add or remove users" />
+              </PermissionBlock>
+            }
           />
         </Chooser>
       </Dropdown>
@@ -47,9 +79,9 @@ export default class MemberRole extends React.PureComponent {
   label(value) {
     switch (value) {
       case TeamMemberRoleConstants.MAINTAINER:
-        return "Maintainer";
+        return "Team Maintainer";
       case TeamMemberRoleConstants.MEMBER:
-        return "Member";
+        return "Team Member";
     }
   }
 }
