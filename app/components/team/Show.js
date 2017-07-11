02fd42b8d70b125c693157064bd9e97af62e186c
Tim Lucas <t@toolmantim.com>
2017-07-11 15:32:39 +1000

    Remove old team label props

--- app/components/team/Show.js
+++ app/components/team/Show.js
@@ -18,8 +18,6 @@ class TeamShow extends React.Component {
       name: PropTypes.string.isRequired,
       description: PropTypes.string,
       slug: PropTypes.string.isRequired,
-      privacy: PropTypes.string.isRequired,
-      isDefaultTeam: PropTypes.bool.isRequired,
       members: PropTypes.shape({
         count: PropTypes.number
       }).isRequired,
