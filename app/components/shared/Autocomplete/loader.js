0ada94f8153c1a7d2f7d902c8a1b753fa9732cb4
Keith Pitt <me@keithpitt.com>
2017-07-12 15:07:22 +1000

    Added a  component to the AutocompleteDialog gear

---
+++ app/components/shared/Autocomplete/loader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import Spinner from '../../shared/Spinner';
+
+export default class Loader extends React.PureComponent {
+  static displayName = "Autocomplete.Loader";
+
+  render() {
+    return (
+      <li className="center py4"><Spinner /></li>
+    );
+  }
+}
