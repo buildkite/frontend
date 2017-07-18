646124db3fb8dc66a1b6d63f7f43647712873a7d
Jessica Stokes <hello@jessicastokes.net>
2017-07-18 14:40:35 -0700

    Simplify RevealableDownChevron

--- app/components/shared/Icon/RevealableDownChevron.js
+++ app/components/shared/Icon/RevealableDownChevron.js
@@ -1,10 +1,11 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import Icon from '../Icon';
 
-const RotatableIcon = styled(Icon)`
+const RevealableDownChevron = styled(Icon).attrs({
+  icon: "chevron-right"
+})`
   transform: rotate(${(props) => props.revealed ? -90 : 90}deg);
   trasform-origin: center 0;
   transition: transform 300ms;
@@ -17,31 +18,18 @@ const RotatableIcon = styled(Icon)`
   }
 `;
 
-export default class RevealableDownChevron extends React.PureComponent {
-  static propTypes = {
-    revealed: PropTypes.bool,
-    size: PropTypes.number,
-    strokeWidth: PropTypes.number,
-    className: PropTypes.string,
-    style: PropTypes.object
-  };
+RevealableDownChevron.propTypes = {
+  revealed: PropTypes.bool,
+  size: PropTypes.number,
+  strokeWidth: PropTypes.number,
+  className: PropTypes.string,
+  style: PropTypes.object
+};
 
-  static defaultProps = {
-    revealed: false,
-    size: 14,
-    strokeWidth: 2
-  }
+RevealableDownChevron.defaultProps = {
+  revealed: false,
+  size: 14,
+  strokeWidth: 2
+};
 
-  render() {
-    return (
-      <RotatableIcon
-        icon="chevron-right"
-        revealed={this.props.revealed}
-        size={this.props.size}
-        strokeWidth={this.props.strokeWidth}
-        className={this.props.className}
-        style={this.props.style}
-      />
-    );
-  }
-}
+export default RevealableDownChevron;
