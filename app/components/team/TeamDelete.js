cfe6b20f30d570903ff2899f02fb81e7319df79c
Keith Pitt <me@keithpitt.com>
2017-07-19 10:45:44 +1000

    Fixed eslint error

--- app/components/team/TeamDelete.js
+++ app/components/team/TeamDelete.js
@@ -41,16 +41,16 @@ class TeamDelete extends React.Component {
           </Panel.Section>
         </Panel>
       );
-    } else {
-      return (
-        <Panel className="mt3">
-          <Panel.Header>Delete Team</Panel.Header>
-          <Panel.Section>
-            <p>You're not allowed to delete this team. Please contact an Organization Administrator if you need it removed.</p>
-          </Panel.Section>
-        </Panel>
-      );
     }
+    return (
+      <Panel className="mt3">
+        <Panel.Header>Delete Team</Panel.Header>
+        <Panel.Section>
+          <p>You’re not allowed to delete this team. Please contact an Organization Administrator if you need it removed.</p>
+        </Panel.Section>
+      </Panel>
+    );
+
   }
 
   handleButtonClick = () => {
