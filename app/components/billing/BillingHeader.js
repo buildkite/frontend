2d4d8a1616f0533ec1e45e82f07d55cd5cd399d0
Keith Pitt <me@keithpitt.com>
2017-07-20 17:22:39 +1000

    Added a basic header for the new billing page

---
+++ app/components/billing/BillingHeader.js
@@ -0,0 +1,63 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import Relay from 'react-relay/classic';
+import classNames from 'classnames';
+
+import Icon from '../shared/Icon';
+import PageHeader from '../shared/PageHeader';
+import TabControl from '../shared/TabControl';
+
+class BillingHeader extends React.PureComponent {
+  static propTypes = {
+    organization: PropTypes.shape({
+      slug: PropTypes.string.isRequired
+    }).isRequired
+  };
+
+  render() {
+    return (
+      <div>
+        <PageHeader>
+          <PageHeader.Icon>
+            <Icon
+              icon="billing"
+              style={{ width: 40, height: 40 }}
+            />
+          </PageHeader.Icon>
+          <PageHeader.Title>
+            Billing
+          </PageHeader.Title>
+          <PageHeader.Description>
+            Manage your organization’s billing information, change your plan, and view your invoices
+          </PageHeader.Description>
+        </PageHeader>
+
+        <TabControl>
+          {this.renderTab(`/organizations/${this.props.organization.slug}/billing`, "Overview")}
+          {this.renderTab(`/organizations/${this.props.organization.slug}/billing/invoices`, "Invoices")}
+          {this.renderTab(`/organizations/${this.props.organization.slug}/billing/settings`, "Settings")}
+        </TabControl>
+      </div>
+    );
+  }
+
+  renderTab(href, title) {
+    const classes = classNames({
+      "active": (window.location.pathname === href)
+    });
+
+    return (
+      <TabControl.Tab href={href} className={classes}>{title}</TabControl.Tab>
+    );
+  }
+}
+
+export default Relay.createContainer(BillingHeader, {
+  fragments: {
+    organization: () => Relay.QL`
+      fragment on Organization {
+        slug
+      }
+    `
+  }
+});
