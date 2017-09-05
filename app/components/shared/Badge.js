7dad227396a614ae3fa35c22993d70346447593d
Samuel Cochran <sj26@sj26.com>
2017-09-05 16:46:07 +1000

    Add Badge title as property

--- app/components/shared/Badge.js
+++ app/components/shared/Badge.js
@@ -6,6 +6,7 @@ export default class Badge extends React.PureComponent {
   static propTypes = {
     children: PropTypes.node,
     className: PropTypes.string,
+    title: PropTypes.string,
     outline: PropTypes.bool
   };
 
