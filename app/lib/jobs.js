70770cd7136ad58ba1294a77e8cf564487326072
Tim Lucas <t@toolmantim.com>
2017-10-31 16:58:09 +1100

    RIP jobWaitTime

--- app/lib/jobs.js
+++ app/lib/jobs.js
@@ -31,21 +31,3 @@ export function jobTime(job: Object) {
 
   return time;
 }
-
-export function jobWaitTime(job: Object) {
-  const { state, scheduledAt, startedAt } = job;
-  const time = {};
-
-  // job is blocked, don't bother to calculate
-  if (state === JobStates.BLOCKED) {
-    return time;
-  }
-
-  time.from = scheduledAt;
-
-  if (startedAt) {
-    time.to = startedAt;
-  }
-
-  return time;
-}
