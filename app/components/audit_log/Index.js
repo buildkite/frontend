8fb0c644aa0df2f1ce3bf96b15e7f6383592bfc4
Keith Pitt <me@keithpitt.com>
2018-01-09 10:54:27 +0800

    Fixed the "Load More" button on the Audit Events list

--- app/components/audit_log/Index.js
+++ app/components/audit_log/Index.js
@@ -23,7 +23,7 @@ type Props = {
 };
 
 type State = {
-  loading: boolean
+  loadingMore: boolean
 };
 
 class AuditLogList extends React.PureComponent<Props, State> {
@@ -38,7 +38,7 @@ class AuditLogList extends React.PureComponent<Props, State> {
   };
 
   state = {
-    loading: true
+    loadingMore: false
   };
 
   componentDidMount() {
@@ -50,11 +50,7 @@ class AuditLogList extends React.PureComponent<Props, State> {
     return (
       <Panel>
         {this.renderEvents()}
-        <ShowMoreFooter
-          connection={this.props.organization.auditEvents}
-          loading={this.state.loading}
-          onShowMore={this.handleShowMoreAuditEvents}
-        />
+        {this.renderFooter()}
       </Panel>
     );
   }
@@ -87,9 +83,20 @@ class AuditLogList extends React.PureComponent<Props, State> {
       </Panel.Section>
     );
   }
+  renderFooter() {
+    if(this.props.organization.auditEvents) {
+      return (
+        <ShowMoreFooter
+          connection={this.props.organization.auditEvents}
+          loading={this.state.loadingMore}
+          onShowMore={this.handleShowMoreAuditEvents}
+        />
+      )
+    }
+  }
 
   handleShowMoreAuditEvents = () => {
-    this.setState({ loading: true });
+    this.setState({ loadingMore: true });
 
     this.props.relay.setVariables(
       {
@@ -97,7 +104,7 @@ class AuditLogList extends React.PureComponent<Props, State> {
       },
       (readyState) => {
         if (readyState.done) {
-          this.setState({ loading: false });
+          this.setState({ loadingMore: false });
         }
       }
     );
