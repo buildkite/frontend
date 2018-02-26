030b1a8592729eeb3e355165b5da9891ec759719
Keith Pitt <me@keithpitt.com>
2018-02-26 12:25:28 +0800

    Load in the GraphQL schema for the explorer and show a spinner

--- app/components/user/graphql/GraphQLExplorer.js
+++ app/components/user/graphql/GraphQLExplorer.js
@@ -6,12 +6,47 @@ import DocumentTitle from "react-document-title";
 import PageHeader from "../../shared/PageHeader";
 import TabControl from "../../shared/TabControl";
 import Icon from "../../shared/Icon";
+import Spinner from "../../shared/Spinner";
+
+import { fetchAndBuildGraphQLSchema } from "./graphql";
 
 type Props = {
   children: React$Node
 };
 
-class GraphQLExplorer extends React.PureComponent<Props> {
+type State = {
+  schemaLoaded: boolean,
+  loadingIsSlow: boolean,
+  schemaError: string
+}
+
+const MIN_HEIGHT = 300;
+
+class GraphQLExplorer extends React.PureComponent<Props, State> {
+  state = {
+    schemaLoaded: false,
+    loadingIsSlow: false,
+    schemaError: null
+  };
+
+  componentDidMount() {
+    // Even though we don't use the schema for anything in here, we're just
+    // preloading it so when the childen load, they dont have to do anything.
+    fetchAndBuildGraphQLSchema().then(() => {
+      this.setState({ schemaLoaded: true });
+    }).catch((error) => {
+      this.setState({ schemaError: error });
+    });
+
+    // After a few moments, set `loadingIsSlow` to true. If we're still loading
+    // things at this point, the spinner will show up. We don't need/want to
+    // show the spinner if the dependencies have been loaded in from the cache
+    // right away.
+    setTimeout(() => {
+      this.setState({ loadingIsSlow: true });
+    }, 1000);
+  }
+
   render() {
     return (
       <DocumentTitle title={`GraphQL Console`}>
@@ -38,8 +73,30 @@ class GraphQLExplorer extends React.PureComponent<Props> {
   }
 
   renderContent() {
+    if (this.state.schemaError) {
+      return (
+	<div className="flex items-center justify-center" style={{ minHeight: MIN_HEIGHT }}>
+	  <span className="red">There was an error loading the GraphQL Schema. Please try again.</span>
+	</div>
+      );
+    }
+
+    if (!this.state.schemaLoaded) {
+      if (this.state.loadingIsSlow) {
+        return (
+          <div className="flex items-center justify-center" style={{ minHeight: MIN_HEIGHT }}>
+            <Spinner className="mr1" /> Loading GraphQL Explorer…
+          </div>
+        );
+      } else {
+        return (
+          <div style={{ minHeight: MIN_HEIGHT }}>&nbsp;</div>
+        )
+      }
+    }
+
     return (
-      <div>
+      <div style={{ minHeight: MIN_HEIGHT }}>
         <TabControl>
           <TabControl.Tab to={`/user/graphql/console`}>Console</TabControl.Tab>
           <TabControl.Tab to={`/user/graphql/documentation`}>Documentation</TabControl.Tab>
@@ -48,7 +105,7 @@ class GraphQLExplorer extends React.PureComponent<Props> {
 
         {this.props.children}
       </div>
-    );
+    )
   }
 }
 
---
+++ app/components/user/graphql/graphql.js
@@ -0,0 +1,52 @@
+import { window } from 'global';
+import { introspectionQuery, buildClientSchema } from 'graphql';
+
+let cachedSchema = null;
+
+export function getGraphQLSchema() {
+  if (cachedSchema) {
+    return cachedSchema;
+  }
+  throw new Error("Schema needs to be fetched first");
+
+}
+
+export function setGraphQLSchemaFromJSON(json) {
+  cachedSchema = buildClientSchema(json);
+}
+
+export function fetchAndBuildGraphQLSchema() {
+  return new Promise(function(resolve, reject) {
+    // If we've already loaded the schema, let's resolve right away.
+    if (cachedSchema) {
+      resolve(cachedSchema);
+    }
+
+    // Fetch the schema using the introspection query provided by GraphQL
+    executeGraphQLQuery({ query: introspectionQuery }).then((result) => {
+      // Now convert it to JSON
+      result.json().then((json) => {
+	if (json && json.data) {
+	  cachedSchema = buildClientSchema(json.data);
+	  resolve(cachedSchema);
+	} else {
+	  reject("Failed to find `data` payload in GraphQL response");
+	}
+      }).catch((error) => {
+	reject("Failed to parse JSON response");
+      });
+    }).catch(function(error) {
+      reject("Request for GraphQL schema failed");
+    });
+  });
+}
+
+export function executeGraphQLQuery(params) {
+  return fetch(window._graphql['url'], {
+    method: 'post',
+    body: JSON.stringify(params),
+    credentials: "same-origin",
+    headers: window._graphql["headers"]
+  });
+}
+
