811bf68a1ccfe340bfe61897bb1eafe297677edf
Keith Pitt <me@keithpitt.com>
2018-03-15 12:59:01 +0800

    Simplify `jobCommandOneliner` and remove trailing `&&`

--- app/lib/jobCommandOneliner.js
+++ app/lib/jobCommandOneliner.js
@@ -5,15 +5,9 @@
 //
 // For example:
 //   jobCommandOneliner("\n\n\nls -l\n\necho 'oh hai'\n")
-//   => "ls -l && echo 'oh hai' &&"
+//   => "ls -l && echo 'oh hai'"
 export default function jobCommandOneliner(command: ?string): ?string {
   if (command) {
-    return command.
-      // Remove any leading whitespace (such as multiple new lines before any
-      // commands)
-      replace(/^(\n|\s)+/, "").
-      // We want && to appear when you hit enter in the command text area,
-      // so we collapse all trailing whitespace to a single &&
-      replace(/\s*\n+\s*/g, " && ");
+    return command.trim().replace(/\s*\n+\s*/g, " && ");
   }
 }
