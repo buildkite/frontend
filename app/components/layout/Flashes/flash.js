d69cb5ea26f5d2ff5055d8bdd35a37ecb9475edf
Jessica Stokes <hello@jessicastokes.net>
2018-06-01 17:12:20 -0700

    Flow in Flash row

--- app/components/layout/Flashes/flash.js
+++ app/components/layout/Flashes/flash.js
@@ -1,18 +1,19 @@
+// @flow
+
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import FlashesStore from '../../../stores/FlashesStore';
 
-class Flash extends React.PureComponent {
-  static propTypes = {
-    flash: PropTypes.shape({
-      type: PropTypes.string.isRequired,
-      message: PropTypes.node.isRequired
-    }),
-    onRemoveClick: PropTypes.func.isRequired
-  };
+type Props = {
+  flash: {
+    type: string,
+    message: React$Node
+  },
+  onRemoveClick: Function
+};
 
+class Flash extends React.PureComponent<Props> {
   render() {
     const classes = classNames("flex items-center mb2 border rounded border-dark-gray", {
       "border-red red": this.props.flash.type === FlashesStore.ERROR,
@@ -21,8 +22,19 @@ class Flash extends React.PureComponent {
 
     return (
       <div className={classes}>
-        <div className="flex-auto px4 py3" data-flash-message={true}>{this.props.flash.message}</div>
-        <button className="btn px4 py3" onClick={this.handleCloseClick} data-flash-close={true}>Close</button>
+        <div
+          className="flex-auto px4 py3"
+          data-flash-message={true}
+        >
+          {this.props.flash.message}
+        </div>
+        <button
+          className="btn px4 py3"
+          onClick={this.handleCloseClick}
+          data-flash-close={true}
+        >
+          Close
+        </button>
       </div>
     );
   }
