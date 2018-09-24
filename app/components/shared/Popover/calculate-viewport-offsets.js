93b4da57221f6acd761b44641215ac5401da3233
Jessica Stokes <hello@jessicastokes.net>
2018-09-24 13:00:28 -0700

    Add support for iPhone X "safe area"

--- app/components/shared/Popover/calculate-viewport-offsets.js
+++ app/components/shared/Popover/calculate-viewport-offsets.js
@@ -2,20 +2,47 @@
 
 const { abs, min, max } = Math;
 
-// margin (in pixels) to maintain around automatically-positioned dropdowns
-const SCREEN_MARGIN = 10;
-
-const combineAxisOffsets = (lowOffset, highOffset) => (
-  abs(min(lowOffset - SCREEN_MARGIN, 0)) - abs(max(highOffset + SCREEN_MARGIN, 0))
-);
-
 type ViewportOffsets = {
   offsetX: number,
   offsetY: number,
   width: number
 };
 
+// margin (in pixels) to maintain around automatically-positioned dropdowns
+const SCREEN_MARGIN = 10;
+
+let safeAreaProbeNode: ?HTMLDivElement;
+
+const detectSafeArea = () => {
+  if (!safeAreaProbeNode) {
+    safeAreaProbeNode = document.createElement('div');
+    safeAreaProbeNode.setAttribute('style',
+      'padding: constant(safe-area-inset-top) constant(safe-area-inset-right) constant(safe-area-inset-bottom) constant(safe-area-inset-left);' +
+      'padding: env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left);' +
+      'position: absolute;' +
+      'opacity: 0;' +
+      'visibility: hidden;'
+    );
+    document.body && document.body.appendChild(safeAreaProbeNode);
+  }
+
+  let { paddingLeft: left, paddingRight: right, paddingTop: top, paddingBottom: bottom } = getComputedStyle(safeAreaProbeNode);
+
+  left = parseFloat(left);
+  right = parseFloat(right);
+  top = parseFloat(top);
+  bottom = parseFloat(bottom);
+
+  return { left, right, top, bottom };
+};
+
+const combineAxisOffsets = (lowOffset, highOffset, computedScreenMargin = SCREEN_MARGIN) => (
+  abs(min(lowOffset - SCREEN_MARGIN, 0)) - abs(max(highOffset + computedScreenMargin, 0))
+);
+
 export default (requestedWidth: number = 250, offsetNode: HTMLElement): ViewportOffsets => {
+  const safeArea = detectSafeArea();
+  const computedScreenMargin = (safeArea.left + safeArea.right) / 2 + SCREEN_MARGIN;
   const windowWidth = window.innerWidth;
   const { left: offsetNodeLeft, width: offsetNodeWidth, height: offsetNodeHeight } = offsetNode.getBoundingClientRect();
 
@@ -23,7 +50,7 @@ export default (requestedWidth: number = 250, offsetNode: HTMLElement): Viewport
 
   // automatically shrink the popover to fit the screen if the screen is too small
   // this shouldn't be needed often, but seems worth keeping just in case!
-  const width = min(requestedWidth, windowWidth - (SCREEN_MARGIN * 2));
+  const width = min(requestedWidth, windowWidth - (computedScreenMargin * 2));
 
   // if `leftOffset` is < 0, we need to shift the popup to the right
   const leftOffset = offsetNodeCenterX - (width / 2);
@@ -32,7 +59,7 @@ export default (requestedWidth: number = 250, offsetNode: HTMLElement): Viewport
   const rightOffset = offsetNodeCenterX + (width / 2) - windowWidth;
 
   // calculate the overall offset required to stay on-screen
-  const offsetX = combineAxisOffsets(leftOffset, rightOffset);
+  const offsetX = combineAxisOffsets(leftOffset, rightOffset, computedScreenMargin);
   const offsetY = offsetNodeHeight;
 
   return {
