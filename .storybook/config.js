74f558240a02b97dfd9789769319f5b4d2ebabdc
Justin Morris <desk@pixelbloom.com>
2018-10-25 22:07:38 +1100

    Start Storybook in CI mode so we don't open the damn browser every time

--- .storybook/config.js
+++ .storybook/config.js
@@ -9,9 +9,7 @@ import centered from '@storybook/addon-centered';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
-}
+const loadStories = () => req.keys().forEach(req);
 
 // init the addons
 addDecorator(checkA11y);
