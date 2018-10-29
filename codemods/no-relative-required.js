5cee898b8294087576f56ab00801829164254fd7
Jessica Stokes <hello@jessicastokes.net>
2018-10-29 10:25:15 -0700

    yarn lint --fix && remove unused vars

--- codemods/no-relative-required.js
+++ codemods/no-relative-required.js
@@ -1,4 +1,4 @@
-import path, {resolve, normalize, relative, dirname, parse} from 'path';
+import path, { normalize, parse } from 'path';
 
 export const parser = 'flow';
 
@@ -8,35 +8,35 @@ function isRelativePath(path) {
 
 // This codemod rewrites relative imports to 'absolute', module style imports.
 export default function removeRelativeImports(file, api, options) {
-  const {dir: fileDir} = parse(file.path)
-  const {jscodeshift} = api;
-  const printOptions = options.printOptions || {quote: 'single'};
-  const root = jscodeshift(file.source)
+  const { dir: fileDir } = parse(file.path);
+  const { jscodeshift } = api;
+  const printOptions = options.printOptions || { quote: 'single' };
+  const root = jscodeshift(file.source);
   const nodesToUpdate = new Set();
 
   root
     .find(jscodeshift.ImportDeclaration)
     .find(jscodeshift.Literal)
-    .filter(node => isRelativePath(node.value.value))
-    .forEach(path => nodesToUpdate.add(path));
+    .filter((node) => isRelativePath(node.value.value))
+    .forEach((path) => nodesToUpdate.add(path));
 
   root
     .find(jscodeshift.ExportNamedDeclaration)
-    .filter(path => path.value.source !== null)
+    .filter((path) => path.value.source !== null)
     .find(jscodeshift.Literal)
-    .filter(node => isRelativePath(node.value.value))
-    .forEach(path => nodesToUpdate.add(path));
+    .filter((node) => isRelativePath(node.value.value))
+    .forEach((path) => nodesToUpdate.add(path));
 
   root
     .find(jscodeshift.ExportAllDeclaration)
     .find(jscodeshift.Literal)
-    .filter(node => isRelativePath(node.value.value))
-    .forEach(path => nodesToUpdate.add(path));
+    .filter((node) => isRelativePath(node.value.value))
+    .forEach((path) => nodesToUpdate.add(path));
 
-  nodesToUpdate.forEach(node => {
+  nodesToUpdate.forEach((node) => {
     const newPath = normalize(path.join(fileDir, node.value.value));
     jscodeshift(node).replaceWith(() => jscodeshift.literal(newPath));
   });
 
   return nodesToUpdate.size ? root.toSource(printOptions) : null;
-};
+}
