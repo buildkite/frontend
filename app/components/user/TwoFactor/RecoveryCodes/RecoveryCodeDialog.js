ba0fafda162551b09ff710ed45515db7c9755d5b
Keith Pitt <me@keithpitt.com>
2018-11-01 15:11:51 +0800

    Added a "Generate New Recovery Codes" button to the Recovery Codes Dialog

--- app/components/user/TwoFactor/RecoveryCodes/RecoveryCodeDialog.js
+++ app/components/user/TwoFactor/RecoveryCodes/RecoveryCodeDialog.js
@@ -1,10 +1,11 @@
 // @flow
 
 import * as React from 'react';
-import { createFragmentContainer, graphql } from 'react-relay/compat';
+import { createFragmentContainer, graphql, commitMutation } from 'react-relay/compat';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Button from 'app/components/shared/Button';
 import Dialog from 'app/components/shared/Dialog';
+import Panel from 'app/components/shared/Panel';
 import RecoveryCodeList from 'app/components/RecoveryCodeList';
 import type { RecoveryCodeDialog_totp } from './__generated__/RecoveryCodeDialog_totp.graphql';
 
@@ -14,7 +15,8 @@ type Props = {
 };
 
 type State = {
-  copiedRecoveryCodes: boolean
+  copiedRecoveryCodes: boolean,
+  generatingNewCodes: boolean
 };
 
 type RecoveryCodes = $PropertyType<RecoveryCodeDialog_totp, 'recoveryCodes'>;
@@ -28,7 +30,8 @@ function recoveryCodeText(recoveryCodes: RecoveryCodes): ?string {
 
 class RecoveryCodesDialog extends React.PureComponent<Props, State> {
   state = {
-    copiedRecoveryCodes: false
+    copiedRecoveryCodes: false,
+    generatingNewCodes: false
   }
 
   render() {
@@ -37,18 +40,37 @@ class RecoveryCodesDialog extends React.PureComponent<Props, State> {
         <div className="p4">
           <h2 className="m0 h2 semi-bold mb5">Current Recovery Codes</h2>
           <p>Recovery codes are used if you lose access to your code generator.</p>
-          <RecoveryCodeList recoveryCodes={this.props.totp.recoveryCodes} />
           <p>These codes should be treated just like your password. Weʼd suggest saving them into a secure password manager.</p>
-          <CopyToClipboard
-            text={recoveryCodeText(this.props.totp.recoveryCodes)}
-            onCopy={this.handleRecoveryCodeCopy}
+          <Panel className="mb3">
+            <Panel.Section>
+              <CopyToClipboard
+                text={recoveryCodeText(this.props.totp.recoveryCodes)}
+                onCopy={this.handleRecoveryCodeCopy}
+              >
+                <Button theme="default" outline={true}>
+                  {this.state.copiedRecoveryCodes
+                    ? 'Copied!'
+                    : 'Copy'}
+                  </Button>
+                </CopyToClipboard>
+              <RecoveryCodeList
+                recoveryCodes={this.props.totp.recoveryCodes}
+                isLoading={this.state.generatingNewCodes}
+              />
+            </Panel.Section>
+          </Panel>
+          <h2 className="m0 h4 semi-bold mb5">Generate New Recovery Codes</h2>
+          <p>When you generate new recovery codes, your current codes will be removed. Please ensure you copy your new recovery codes after they've been generated.</p>
+          <Button
+            className="col-12"
+            theme="warning"
+            outline={true}
+            onClick={this.handleRegenerateRecoveryCodes}
+            loading={this.state.generatingNewCodes && "Generating New Recovery Codes…"}
+            disabled={this.state.generatingNewCodes}
           >
-            <Button className="col-12" theme="success" outline={this.state.copiedRecoveryCodes}>
-              {this.state.copiedRecoveryCodes
-                ? 'Copied Recovery Codes'
-                : 'Copy Recovery Codes'}
-            </Button>
-          </CopyToClipboard>
+            Generate New Recovery Codes
+          </Button>
         </div>
       </Dialog>
     );
@@ -59,6 +81,33 @@ class RecoveryCodesDialog extends React.PureComponent<Props, State> {
       alert('We couldnʼt put this on your clipboard for you, please copy it manually!');
     }
     this.setState({ copiedRecoveryCodes: true });
+  };
+
+  handleRegenerateRecoveryCodes = (callback?: () => void) => {
+    this.setState({ generatingNewCodes: true });
+
+    commitMutation(this.props.relay.environment, {
+      mutation: graphql`
+          mutation TwoFactorConfigureRecoveryCodeRegenerateMutation($input: TOTPRecoveryCodesRegenerateInput!) {
+            totpRecoveryCodesRegenerate(input: $input) {
+              totp {
+                id
+                recoveryCodes {
+                  id
+                  codes {
+                    code
+                    consumed
+                  }
+                }
+              }
+            }
+          }
+        `,
+      variables: { input: { totpId: this.props.totp.id } },
+      onCompleted: ({ totpRecoveryCodesRegenerate: { totp } }) => {
+        this.setState({ generatingNewCodes: false });
+      }
+    });
   }
 }
 
