6ce2699ace2d23bb5198607e03461fdeed9a84ad
Keith Pitt <me@keithpitt.com>
2018-11-01 20:15:13 +0800

    Rearranged the 2fa bits for a more compact display

--- app/components/user/TwoFactor/RecoveryCodes/index.js
+++ app/components/user/TwoFactor/RecoveryCodes/index.js
@@ -13,39 +13,63 @@ type Props = {
 };
 
 type State = {
-  copiedRecoveryCodes: boolean,
-  generatingNewCodes: boolean
+  generatingNewCodes: boolean,
+  newCodesAvailable: boolean
 };
 
 class RecoveryCodes extends React.PureComponent<Props, State> {
   state = {
-    copiedRecoveryCodes: false,
-    generatingNewCodes: false
+    generatingNewCodes: false,
+    newCodesAvailable: false
   }
 
   render() {
+    let contents;
+    if (this.state.newCodesAvailable) {
+      contents = this.renderNewCodesSection();
+    } else {
+      contents = this.renderGenerateSection();
+    }
+
     return (
       <div className="p4">
-        <h2 className="m0 h2 semi-bold mb5">Recovery Codes</h2>
-        <p>Recovery codes are the only way to get access to your account if you lose access to your authenticator application.</p>
-        <p>Recovery codes should be treated like your password. We suggest saving them in a secure password manager, or printing them and storing them somewhere safe.</p>
-        <RecoveryCodeList
-          recoveryCodes={this.props.totp.recoveryCodes}
-          isLoading={this.state.generatingNewCodes}
-        />
-        <h2 className="m0 mt3 h4 semi-bold mb5">Generate New Recovery Codes</h2>
+        {contents}
+      </div>
+    );
+  }
+
+  renderGenerateSection() {
+    return (
+      <>
+        <h2 className="m0 h2 semi-bold mb5">Generate New Recovery Codes</h2>
         <p>When you generate new recovery codes, your previous codes will no longer be valid. Please ensure you save a copy of your new recovery codes after they’ve been generated.</p>
         <Button
           className="col-12"
-          theme="default"
-          outline={true}
+          theme="success"
           onClick={this.handleRegenerateRecoveryCodes}
           loading={this.state.generatingNewCodes && "Generating New Recovery Codes…"}
           disabled={this.state.generatingNewCodes}
         >
           Generate New Recovery Codes
         </Button>
-      </div>
+      </>
+    );
+  }
+
+  renderNewCodesSection() {
+    return (
+      <>
+        <div className="mb4 border green rounded border-green p3">
+          <div className="bold">Here are your new recovery codes.</div>
+          <div>Your codes are no longer valid. Please make sure you store them in a safe place</div>
+        </div>
+
+        <div className="mt4">
+          <RecoveryCodeList
+            recoveryCodes={this.props.totp.recoveryCodes}
+          />
+        </div>
+      </>
     );
   }
 
@@ -71,7 +95,7 @@ class RecoveryCodes extends React.PureComponent<Props, State> {
         `,
       variables: { input: { totpId: this.props.totp.id } },
       onCompleted: () => {
-        this.setState({ generatingNewCodes: false });
+        this.setState({ newCodesAvailable: true });
       },
       onError: (error) => {
         alert(error);
