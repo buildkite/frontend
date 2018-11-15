79c4cf9d7a517173eb1c5274bbc7895b60185536
Justin Morris <desk@pixelbloom.com>
2018-11-15 13:54:59 +1100

    WIP: testing

---
+++ app/components/organization/__tests__/OrganizationShow_test.js
@@ -0,0 +1,17 @@
+// @flow
+
+import * as React from 'react';
+import renderer from 'react-test-renderer';
+import OrganizationShow from '../Show';
+
+jest.mock('app/components/shared/Icon/svgContent', () => {});
+
+describe('OrganizationShow', () => {
+  test('it renders', () => {
+    const component = renderer.create(
+      <OrganizationShow />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+});
