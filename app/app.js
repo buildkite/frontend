f351049940ef56a1be8f34cedb4169156a0abac6
Justin Morris <desk@pixelbloom.com>
2018-11-22 12:36:57 +1100

    Use full module paths

--- app/app.js
+++ app/app.js
@@ -2,21 +2,19 @@
 
 import React from 'react';
 import Relay from 'react-relay/classic';
-import reactRender from './lib/reactRenderer';
-
-import Routes from './routes';
-
-import EmojiStyleManager from './lib/EmojiStyleManager';
+import reactRender from 'app/lib/reactRenderer';
+import Routes from 'app/routes';
+import EmojiStyleManager from 'app/lib/EmojiStyleManager';
 
 // Detect and adjust for custom emoji scaling
 EmojiStyleManager.apply();
 
-require("./css/main.css");
+require("app/css/main.css");
 
 // Toggle on development features
 if (process.env.NODE_ENV === "development") {
   require('react-type-snob').default(React);
-  require('./lib/Logger').default.enable();
+  require('app/lib/Logger').default.enable();
   require('react-relay/lib/RelayNetworkDebug').init();
 }
 
@@ -37,60 +35,60 @@ window["Webpack"] = {
     "react-relay": require("react-relay/classic"),
     "react-addons-pure-render-mixin": require("react-addons-pure-render-mixin"),
 
-    "components/billing/BillingHeader": require("./components/billing/BillingHeader").default,
-    "components/build/AnnotationsList": require("./components/build/AnnotationsList").default,
-    "components/build/AvatarWithUnknownEmailPrompt": require("./components/build/AvatarWithUnknownEmailPrompt").default,
-    "components/build/Header": require("./components/build/Header").default,
-    "components/build/Show": require("./components/build/Show").default,
-    "components/build/StateSwitcher": require("./components/build/StateSwitcher").default,
-    "components/icons/BuildState": require("./components/icons/BuildState").default,
-    "components/layout/Footer": require("./components/layout/Footer").default,
-    "components/layout/Navigation": require("./components/layout/Navigation").default,
-    "components/layout/Flashes": require("./components/layout/Flashes").default,
-    "components/organization/AgentsCount": require("./components/organization/AgentsCount").default,
-    "components/organization/SettingsMenu": require("./components/organization/SettingsMenu").default,
-    "components/pipeline/Header": require("./components/pipeline/Header").default,
-    "components/pipeline/Teams": require("./components/pipeline/Teams").default,
-    "components/pipeline/SettingsMenu": require("./components/pipeline/SettingsMenu").default,
-    "components/PipelinesWelcome": require("./components/organization/Welcome").default,
-    "components/shared/BuildStatusDescription": require("./components/shared/BuildStatusDescription").default,
-    "components/shared/Button": require("./components/shared/Button").default,
-    "components/shared/CollapsableFormField": require("./components/shared/CollapsableFormField").default,
-    "components/shared/Duration": require("./components/shared/Duration").default,
-    "components/shared/Emojify": require("./components/shared/Emojify").default,
-    "components/shared/FormMarkdownEditorField": require("./components/shared/FormMarkdownEditorField").default,
-    "components/shared/FormYAMLEditorField": require("./components/shared/FormYAMLEditorField").default,
-    "components/shared/FormRadioGroup": require("./components/shared/FormRadioGroup").default,
-    "components/shared/FormTextarea": require("./components/shared/FormTextarea").default,
-    "components/shared/FormTextField": require("./components/shared/FormTextField").default,
-    "components/shared/FriendlyTime": require("./components/shared/FriendlyTime").default,
-    "components/shared/Icon": require("./components/shared/Icon").default,
-    "components/shared/UserAvatar": require("./components/shared/UserAvatar").default,
-    "components/user/BuildCountsBreakdown": require("./components/user/BuildCountsBreakdown").default,
-    "components/user/SettingsMenu": require("./components/user/SettingsMenu").default,
-    "components/user/graphql/schema": require("./components/user/graphql/schema"),
-    "lib/AssetUploader": require("./lib/AssetUploader").default,
-    "lib/Bugsnag": require("./lib/Bugsnag"),
-    "lib/builds": require("./lib/builds"),
-    "lib/commits": require("./lib/commits"),
-    "lib/date": require("./lib/date"),
-    "lib/jobCommandOneliner": require("./lib/jobCommandOneliner").default,
-    "lib/jobs": require("./lib/jobs"),
-    "lib/Logger": require("./lib/Logger").default,
-    "lib/number": require("./lib/number"),
-    "lib/parseEmoji": require("./lib/parseEmoji").default,
-    "lib/reactRenderer": require("./lib/reactRenderer").default,
-    "lib/RelayPreloader": require("./lib/RelayPreloader").default,
-    "lib/BootstrapTooltipMixin": require('./lib/BootstrapTooltipMixin').default,
-    "queries/Agent": require("./queries/Agent"),
-    "queries/Build": require("./queries/Build"),
-    "queries/Organization": require("./queries/Organization"),
-    "queries/Pipeline": require("./queries/Pipeline"),
-    "queries/PipelineSchedule": require("./queries/PipelineSchedule"),
-    "queries/Team": require("./queries/Team"),
-    "queries/Viewer": require("./queries/Viewer"),
-    "stores/FlashesStore": require("./stores/FlashesStore").default,
-    "stores/PusherStore": require("./stores/PusherStore").default
+    "components/billing/BillingHeader": require("app/components/billing/BillingHeader").default,
+    "components/build/AnnotationsList": require("app/components/build/AnnotationsList").default,
+    "components/build/AvatarWithUnknownEmailPrompt": require("app/components/build/AvatarWithUnknownEmailPrompt").default,
+    "components/build/Header": require("app/components/build/Header").default,
+    "components/build/Show": require("app/components/build/Show").default,
+    "components/build/StateSwitcher": require("app/components/build/StateSwitcher").default,
+    "components/icons/BuildState": require("app/components/icons/BuildState").default,
+    "components/layout/Footer": require("app/components/layout/Footer").default,
+    "components/layout/Navigation": require("app/components/layout/Navigation").default,
+    "components/layout/Flashes": require("app/components/layout/Flashes").default,
+    "components/organization/AgentsCount": require("app/components/organization/AgentsCount").default,
+    "components/organization/SettingsMenu": require("app/components/organization/SettingsMenu").default,
+    "components/pipeline/Header": require("app/components/pipeline/Header").default,
+    "components/pipeline/Teams": require("app/components/pipeline/Teams").default,
+    "components/pipeline/SettingsMenu": require("app/components/pipeline/SettingsMenu").default,
+    "components/PipelinesWelcome": require("app/components/organization/Welcome").default,
+    "components/shared/BuildStatusDescription": require("app/components/shared/BuildStatusDescription").default,
+    "components/shared/Button": require("app/components/shared/Button").default,
+    "components/shared/CollapsableFormField": require("app/components/shared/CollapsableFormField").default,
+    "components/shared/Duration": require("app/components/shared/Duration").default,
+    "components/shared/Emojify": require("app/components/shared/Emojify").default,
+    "components/shared/FormMarkdownEditorField": require("app/components/shared/FormMarkdownEditorField").default,
+    "components/shared/FormYAMLEditorField": require("app/components/shared/FormYAMLEditorField").default,
+    "components/shared/FormRadioGroup": require("app/components/shared/FormRadioGroup").default,
+    "components/shared/FormTextarea": require("app/components/shared/FormTextarea").default,
+    "components/shared/FormTextField": require("app/components/shared/FormTextField").default,
+    "components/shared/FriendlyTime": require("app/components/shared/FriendlyTime").default,
+    "components/shared/Icon": require("app/components/shared/Icon").default,
+    "components/shared/UserAvatar": require("app/components/shared/UserAvatar").default,
+    "components/user/BuildCountsBreakdown": require("app/components/user/BuildCountsBreakdown").default,
+    "components/user/SettingsMenu": require("app/components/user/SettingsMenu").default,
+    "components/user/graphql/schema": require("app/components/user/graphql/schema"),
+    "lib/AssetUploader": require("app/lib/AssetUploader").default,
+    "lib/Bugsnag": require("app/lib/Bugsnag"),
+    "lib/builds": require("app/lib/builds"),
+    "lib/commits": require("app/lib/commits"),
+    "lib/date": require("app/lib/date"),
+    "lib/jobCommandOneliner": require("app/lib/jobCommandOneliner").default,
+    "lib/jobs": require("app/lib/jobs"),
+    "lib/Logger": require("app/lib/Logger").default,
+    "lib/number": require("app/lib/number"),
+    "lib/parseEmoji": require("app/lib/parseEmoji").default,
+    "lib/reactRenderer": require("app/lib/reactRenderer").default,
+    "lib/RelayPreloader": require("app/lib/RelayPreloader").default,
+    "lib/BootstrapTooltipMixin": require('app/lib/BootstrapTooltipMixin').default,
+    "queries/Agent": require("app/queries/Agent"),
+    "queries/Build": require("app/queries/Build"),
+    "queries/Organization": require("app/queries/Organization"),
+    "queries/Pipeline": require("app/queries/Pipeline"),
+    "queries/PipelineSchedule": require("app/queries/PipelineSchedule"),
+    "queries/Team": require("app/queries/Team"),
+    "queries/Viewer": require("app/queries/Viewer"),
+    "stores/FlashesStore": require("app/stores/FlashesStore").default,
+    "stores/PusherStore": require("app/stores/PusherStore").default
   },
 
   require: function(module) {
@@ -125,7 +123,7 @@ if (window._graphql) {
 
 // Setup the PusherStore
 if (window._pusher) {
-  const pusherStore = require("./stores/PusherStore").default;
+  const pusherStore = require("app/stores/PusherStore").default;
   pusherStore.configure(window._pusher["key"], window._pusher["options"]);
   for (const channel of window._pusher["channels"]) {
     pusherStore.listen(channel);
