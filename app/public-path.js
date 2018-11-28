326c19a7519181e347d2629fd4d8bdd8b2f8b3d6
Samuel Cochran <sj26@sj26.com>
2018-11-28 13:42:42 +1100

    Don't override webpack dev server public path
    Otherwise it fails to dynamically load chunks like code mirror.

--- app/public-path.js
+++ app/public-path.js
@@ -1,9 +1,14 @@
 /* global __webpack_public_path__:true */
 /* exported __webpack_public_path__ */
 
-// Allow overriding the webpack public path
+// Allow specifying the webpack public path
+//
+// When running webpack dev server in development it provides a public path,
+// and we should use it.
+//
 // Must be done in a module so that it is executed before all other imports.
+//
 // See https://webpack.js.org/guides/public-path/#on-the-fly
-if (window._frontendPublicPath) {
+if (__webpack_public_path__ === "" && window._frontendPublicPath) {
   __webpack_public_path__ = window._frontendPublicPath;
 }
