9f2c32458c71ee66c3cc623cb8f3aa1c3a40a413
Matthew Draper <matthew@trebex.net>
2019-01-18 16:32:48 +1030

    Extend the "conditional" syntax mode to cover implemented behaviour

--- app/components/shared/FormConditionEditorField/codemirror.js
+++ app/components/shared/FormConditionEditorField/codemirror.js
@@ -14,13 +14,42 @@ import 'app/css/codemirror.css';
 CodeMirror.defineSimpleMode("conditional", {
   // The start state contains the rules that are intially used
   start: [
-    { regex: /^\/\/.*/, token: "comment" },
-    { regex: /["'](?:[^\\]|\\.)*?(?:["']|$)/, token: "string" },
-    { regex: /[\/](?:[^\\]|\\.)+(?:[\/])/, token: "regex" },
-    { regex: /env/, token: "keyword" },
-    { regex: /[\(\)]/, token: "punctuation" },
+    { regex: /\/\/.*/, token: "comment" },
+    { regex: /'(?:[^\\']|\\.)*?(?:'|$)/, token: "string" },
+    { regex: /"/, token: "string", push: "quotestring" },
+    { regex: /\/(?:[^\\/]|\\.)+\/[a-z]*/, token: "regex" },
+    { regex: /[().,]/, token: "punctuation" },
     { regex: /[!=~%|&]+/, token: "operator" },
-    { regex: /[a-zA-Z0-9_]+/, token: "string" },
+    { regex: /[a-zA-Z_][a-zA-Z0-9_]*/, token: "variable" },
+    { regex: /[0-9]+(?:\.[0-9]+)?/, token: "number" },
+    { regex: /\$[a-zA-Z_][a-zA-Z0-9_]*/, token: "variable-2" },
+    { regex: /\$\{/, token: "variable-3", push: "shellbrace" },
+  ],
+
+  quotestring: [
+    { regex: /"/, token: "string", pop: true },
+    { regex: /\$[a-zA-Z_][a-zA-Z0-9_]*/, token: "variable-2" },
+    { regex: /\$\{/, token: "variable-3", push: "shellbrace" },
+    { regex: /\$\$|\\[0-7]{1,3}|\\x[0-9a-fA-F]{2}|\\./, token: "string-2" },
+    { regex: /[^"$\\]+|./, token: "string" },
+  ],
+
+  shellbrace: [
+    { regex: /\}/, token: "variable-3", pop: true },
+    { regex: /:?[+-]/, token: "variable-3", next: "shellstring" },
+    { regex: /[:?]/, token: "variable-3" },
+    { regex: /[0-9]+/, token: "number" },
+    { regex: /[a-zA-Z_][a-zA-Z0-9_]*/, token: "variable-2" },
+  ],
+
+  shellstring: [
+    { regex: /\}/, token: "variable-3", pop: true },
+    { regex: /\$[a-zA-Z_][a-zA-Z0-9_]*/, token: "variable-2" },
+    { regex: /\$\{/, token: "variable-3", push: "shellbrace" },
+    { regex: /'(?:[^\\']|\\.)*?(?:'|$)/, token: "string" },
+    { regex: /"/, token: "string", push: "quotestring" },
+    { regex: /\$\$|\\[0-7]{1,3}|\\x[0-9a-fA-F]{2}|\\./, token: "string-2" },
+    { regex: /[^"'$\\}]+|./, token: "string-3" },
   ],
 
   meta: {
